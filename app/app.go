ccc30e562eb96922a9e5ec4fc5605fd67d4c40e8
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2022-09-26 17:48:09 +0300

    feat: Client registration logic implemented

--- app/app.go
+++ app/app.go
@@ -106,7 +106,7 @@ import (
 )
 
 const (
-	AccountAddressPrefix = "cosmos"
+	AccountAddressPrefix = "soar"
 	Name                 = "soarchain"
 )
 
--- x/poa/keeper/msg_server_gen_client.go
+++ x/poa/keeper/msg_server_gen_client.go
@@ -4,14 +4,44 @@ import (
 	"context"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 	"soarchain/x/poa/types"
 )
 
 func (k msgServer) GenClient(goCtx context.Context, msg *types.MsgGenClient) (*types.MsgGenClientResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// 1. Check if the client exists
+	_, isFound := k.GetClient(ctx, msg.Address)
 
+	if isFound {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "Client is already registered in challenge service.")
+	}
+
+	// 2. Check registration fee
+	registrationFee, _ := sdk.ParseCoinsNormalized("25soar")
+	msgFee, _ := sdk.ParseCoinsNormalized(msg.Fee)
+
+	if msgFee.IsAllLT(registrationFee) {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInsufficientFunds, "Insufficient funds for registration.")
+	}
+
+	// 3. Transfer fee to the protocol, then burn it
+	clientAddr, _ := sdk.AccAddressFromBech32(msg.Address)
+	k.bankKeeper.SendCoinsFromAccountToModule(ctx, clientAddr, types.ModuleName, registrationFee)
+	k.bankKeeper.BurnCoins(ctx, types.ModuleName, registrationFee)
+
+	// 4. Save client into storage
+	newClient := types.Client{
+		Index:       clientAddr.String(),
+		Address:     clientAddr.String(),
+		UniqueId:    "",
+		Score:       sdk.NewInt(100).String(), // Base Score
+		Balance:     "",
+		NetEarnings: sdk.ZeroInt().String(),
+	}
+
+	k.SetClient(ctx, newClient)
 	return &types.MsgGenClientResponse{}, nil
 }
