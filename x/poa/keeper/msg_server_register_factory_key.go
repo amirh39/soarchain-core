cff1bc778d9b57329e5bd7680e65e361b95576d3
Can Dost Yavuz <82905251+candostyavuz@users.noreply.github.com>
2023-02-23 14:59:57 +0300

    fix: test cert files removed

--- x/poa/keeper/msg_server_register_factory_key.go
+++ x/poa/keeper/msg_server_register_factory_key.go
@@ -9,22 +9,6 @@ import (
 	"soarchain/x/poa/types"
 )
 
-// const ecoCertString string = `-----BEGIN CERTIFICATE-----
-// MIIB3TCCAYOgAwIBAgIQYdqh2xopk506MaWSwVjkxjAKBggqhkjOPQQDAjBGMRow
-// GAYDVQQKDBFTb2FyIFJvYm90aWNzIEluYzEoMCYGA1UEAwwfU29hciBSb2JvdGlj
-// cyBTZWN1cmUgRWxlbWVudCBDQTAeFw0yMzAyMjAxMjA1MTBaFw00ODAyMjAxMjA1
-// MTBaMEYxGjAYBgNVBAoMEVNvYXIgUm9ib3RpY3MgSW5jMSgwJgYDVQQDDB9Tb2Fy
-// IFJvYm90aWNzIFNlY3VyZSBFbGVtZW50IENBMFkwEwYHKoZIzj0CAQYIKoZIzj0D
-// AQcDQgAEvCKtYxo9fLS9RvHunODfYwAuPm2NY6rUAMzuTk4N4rpJTDFA1aVva1Yr
-// U2xQ78KHTnTgUGPm/j98oy/nB6KXNqNTMFEwHQYDVR0OBBYEFKlxhLDaJAfFXiVh
-// DKI/FZP1lzb7MB8GA1UdIwQYMBaAFKlxhLDaJAfFXiVhDKI/FZP1lzb7MA8GA1Ud
-// EwEB/wQFMAMBAf8wCgYIKoZIzj0EAwIDSAAwRQIhAIfk8J7lln6CNhZKwWqIgqrS
-// k01jhapY1yHsDjYz32/JAiBRolIuWe6+BigqiseBfxCKPVCHKwE/FaxrWSH6j++D
-// Ow==
-// -----END CERTIFICATE-----`
-const ecoCertFile string = "/Users/candostyavuz/Projects/repo/soarchain-core/x/poa/certs/ecosystem.crt"
-const signerCertFile string = "/Users/candostyavuz/Projects/repo/soarchain-core/x/poa/certs/signer_FFFF.der"
-
 func (k msgServer) RegisterFactoryKey(goCtx context.Context, msg *types.MsgRegisterFactoryKey) (*types.MsgRegisterFactoryKeyResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
@@ -47,10 +31,6 @@ func (k msgServer) RegisterFactoryKey(goCtx context.Context, msg *types.MsgRegis
 	if err != nil {
 		return nil, err
 	}
-	// factoryCert, err := k.CreateX509CertFromFile(signerCertFile)
-	// if err != nil {
-	// 	return nil, err
-	// }
 
 	result, err := k.ValidateX509Cert(factoryCert, masterCert)
 	if err != nil {
