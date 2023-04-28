48b8d2623ce9c9863d9b3682731770c73e543145
hakimemordadi <golnarmordadi@gmail.com>
2023-04-28 19:07:52 +0300

    fix(msg_server_unregister_guard.go): update code to support error handling

--- x/poa/keeper/msg_server_unregister_guard.go
+++ x/poa/keeper/msg_server_unregister_guard.go
@@ -15,19 +15,31 @@ func (k msgServer) UnregisterGuard(goCtx context.Context, msg *types.MsgUnregist
 	// check guard
 	guard, isFound := k.GetGuard(ctx, msg.Creator)
 	if !isFound {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "Guard is not registered, Not authorized!")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[UnregisterGuard][GetGuard] failed. Guard is not registered, Not authorized.")
 	}
 
-	msgSenderAddress, _ := sdk.AccAddressFromBech32(msg.Creator)
+	msgSenderAddress, err := sdk.AccAddressFromBech32(msg.Creator)
+
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "[UnregisterGuard][AccAddressFromBech32] failed. Sender address is not valid.")
+	}
+
+	if guard.V2XChallenger.Address == "" {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "[UnregisterGuard] failed. V2XChallenger address is not valid.")
+	}
 
 	// check v2x challenger
 	v2xChallenger, isFoundV2XChallenger := k.GetChallenger(ctx, guard.V2XChallenger.Address)
 	if isFoundV2XChallenger {
 		stakedAmountStr := v2xChallenger.StakedAmount
-		stakedAmount, _ := sdk.ParseCoinsNormalized(stakedAmountStr)
+		stakedAmount, err := sdk.ParseCoinsNormalized(stakedAmountStr)
+		if err != nil {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "[UnregisterGuard][ParseCoinsNormalized] failed. couldn't parse out a list of coins!"+err.Error())
+		}
+
 		transferErr := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, msgSenderAddress, stakedAmount)
 		if transferErr != nil {
-			return nil, sdkerrors.Wrap(sdkerrors.ErrPanic, "Cannot send coins")
+			return nil, sdkerrors.Wrap(sdkerrors.ErrPanic, "[UnregisterGuard][SendCoinsFromModuleToAccount] failed. Couldn't send coins.")
 		}
 		k.RemoveChallenger(ctx, guard.V2XChallenger.Address)
 	}
@@ -39,7 +51,7 @@ func (k msgServer) UnregisterGuard(goCtx context.Context, msg *types.MsgUnregist
 		stakedAmount, _ := sdk.ParseCoinsNormalized(stakedAmountStr)
 		transferErr := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, msgSenderAddress, stakedAmount)
 		if transferErr != nil {
-			return nil, sdkerrors.Wrap(sdkerrors.ErrPanic, "Cannot send coins")
+			return nil, sdkerrors.Wrap(sdkerrors.ErrPanic, "[UnregisterGuard][SendCoinsFromModuleToAccount] failed. Couldn't send coins.")
 		}
 		k.RemoveChallenger(ctx, guard.V2NChallenger.Address)
 	}
@@ -48,10 +60,14 @@ func (k msgServer) UnregisterGuard(goCtx context.Context, msg *types.MsgUnregist
 	runner, isFoundRunner := k.GetRunner(ctx, guard.Runner.Address)
 	if isFoundRunner {
 		stakedAmountStr := runner.StakedAmount
-		stakedAmount, _ := sdk.ParseCoinsNormalized(stakedAmountStr)
+		stakedAmount, err := sdk.ParseCoinsNormalized(stakedAmountStr)
+		if err != nil {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "[UnregisterGuard][ParseCoinsNormalized] failed. Stake amount not valid"+err.Error())
+		}
+
 		transferErr := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, msgSenderAddress, stakedAmount)
 		if transferErr != nil {
-			return nil, sdkerrors.Wrap(sdkerrors.ErrPanic, "Cannot send coins")
+			return nil, sdkerrors.Wrap(sdkerrors.ErrPanic, "[UnregisterGuard][SendCoinsFromModuleToAccount] failed. Couldn't send coins.")
 		}
 		k.RemoveRunner(ctx, guard.Runner.Address)
 	}
