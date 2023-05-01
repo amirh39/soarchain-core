1ff4f3688b9c2985b35628c5e4ad450e157312c2
hakimemordadi <golnarmordadi@gmail.com>
2023-05-01 18:38:37 +0300

    fix(verifyVRF.go): update code to support error handling

--- x/poa/keeper/verifyVRF.go
+++ x/poa/keeper/verifyVRF.go
@@ -17,17 +17,18 @@ func (k Keeper) VerifyGeneratedNumber(ctx sdk.Context, req *types.QueryVerifyRan
 	var public_key vrf.PublicKey
 	public_key, err := hex.DecodeString(req.Pubkey)
 	if err != nil {
-		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Public Key cannot be decoded")
+		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidPubKey, "[VerifyGeneratedNumber][DecodeString] failed. Couldn't decode given public key by the rquest."+err.Error())
 	}
+
 	message_value := []byte(req.Message)
 	vrv_value, err := hex.DecodeString(req.Vrv)
 	if err != nil {
-		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "VRV Value cannot be decoded")
+		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "[VerifyGeneratedNumber][DecodeString] failed. Couldn't decode given VRV by the request."+err.Error())
 	}
 
 	proof_value, err := hex.DecodeString(req.Proof)
 	if err != nil {
-		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "Proof value cannot be decoded")
+		return false, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "[VerifyGeneratedNumber][DecodeString] failed. Couldn't decode given proof by the request."+err.Error())
 	}
 
 	is_verified := public_key.Verify(message_value, vrv_value, proof_value)
