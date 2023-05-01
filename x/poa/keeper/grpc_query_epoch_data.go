caaf7f1dff09bc6b9f3d16997879e37b81acbeb8
hakimemordadi <golnarmordadi@gmail.com>
2023-05-01 18:20:23 +0300

    fix(x/poa/keeper/grpc_query_epoch_data.go): update code to support error handling

--- x/poa/keeper/grpc_query_epoch_data.go
+++ x/poa/keeper/grpc_query_epoch_data.go
@@ -3,21 +3,22 @@ package keeper
 import (
 	"context"
 
+	"soarchain/x/poa/types"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"soarchain/x/poa/types"
 )
 
 func (k Keeper) EpochData(c context.Context, req *types.QueryGetEpochDataRequest) (*types.QueryGetEpochDataResponse, error) {
 	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
+		return nil, status.Error(codes.InvalidArgument, "[EpochData] failed. Invalid request.")
 	}
 	ctx := sdk.UnwrapSDKContext(c)
 
 	val, found := k.GetEpochData(ctx)
 	if !found {
-		return nil, status.Error(codes.NotFound, "not found")
+		return nil, status.Error(codes.NotFound, "[EpochData][GetEpochData] failed. Couldn't find data for an epoch.")
 	}
 
 	return &types.QueryGetEpochDataResponse{EpochData: val}, nil
