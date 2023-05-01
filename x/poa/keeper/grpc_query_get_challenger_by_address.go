dc0c5cd0c2d4ba22e191846e625475e262b452b1
hakimemordadi <golnarmordadi@gmail.com>
2023-05-01 18:22:13 +0300

    fix(x/poa/keeper/grpc_query_get_challenger_by_address.go): update code to support error handling

--- x/poa/keeper/grpc_query_get_challenger_by_address.go
+++ x/poa/keeper/grpc_query_get_challenger_by_address.go
@@ -6,18 +6,22 @@ import (
 	"soarchain/x/poa/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
 func (k Keeper) GetChallengerByAddress(goCtx context.Context, req *types.QueryGetChallengerByAddressRequest) (*types.QueryGetChallengerByAddressResponse, error) {
 	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
+		return nil, status.Error(codes.InvalidArgument, "[GetChallengerByAddress] failed. Invalid request.")
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	challenger, _ := k.GetChallenger(ctx, req.Address)
+	challenger, found := k.GetChallenger(ctx, req.Address)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, "[GetChallengerByAddress][GetChallenger] failed. Couldn't find a challenger by the address.")
+	}
 
 	return &types.QueryGetChallengerByAddressResponse{Challenger: &challenger}, nil
 }
