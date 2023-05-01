2e707d4ac6c3e40f42accb806e756796df1d9180
hakimemordadi <golnarmordadi@gmail.com>
2023-05-01 18:21:37 +0300

    fix(x/poa/keeper/grpc_query_challenger.go): update code to support error handling

--- x/poa/keeper/grpc_query_challenger.go
+++ x/poa/keeper/grpc_query_challenger.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -14,7 +15,7 @@ import (
 
 func (k Keeper) ChallengerAll(c context.Context, req *types.QueryAllChallengerRequest) (*types.QueryAllChallengerResponse, error) {
 	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
+		return nil, status.Error(codes.InvalidArgument, "[ChallengerAll] failed. Invalid request.")
 	}
 
 	var challengers []types.Challenger
@@ -26,7 +27,7 @@ func (k Keeper) ChallengerAll(c context.Context, req *types.QueryAllChallengerRe
 	pageRes, err := query.Paginate(challengerStore, req.Pagination, func(key []byte, value []byte) error {
 		var challenger types.Challenger
 		if err := k.cdc.Unmarshal(value, &challenger); err != nil {
-			return err
+			sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, "[ChallengerAll][Unmarshal] failed. Couldn't parse the challenger data encoded."+err.Error())
 		}
 
 		challengers = append(challengers, challenger)
@@ -42,7 +43,7 @@ func (k Keeper) ChallengerAll(c context.Context, req *types.QueryAllChallengerRe
 
 func (k Keeper) Challenger(c context.Context, req *types.QueryGetChallengerRequest) (*types.QueryGetChallengerResponse, error) {
 	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "invalid request")
+		return nil, status.Error(codes.InvalidArgument, "[Challenger] failed. Invalid request.")
 	}
 	ctx := sdk.UnwrapSDKContext(c)
 
@@ -51,7 +52,7 @@ func (k Keeper) Challenger(c context.Context, req *types.QueryGetChallengerReque
 		req.Address,
 	)
 	if !found {
-		return nil, status.Error(codes.NotFound, "not found")
+		return nil, status.Error(codes.NotFound, "[Challenger][GetChallenger] failed. Couldn't find a challenger by the given address from the request.")
 	}
 
 	return &types.QueryGetChallengerResponse{Challenger: val}, nil
