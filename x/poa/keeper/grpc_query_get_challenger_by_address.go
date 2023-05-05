0ef0d77084b282a4d70f4cee0a9724eb99cdd4ab
golnar-m <golnar@soarrobotics.com>
2023-05-05 09:08:35 +0300

    refactor(GetChallengerByAddress): cleanup error message

--- x/poa/keeper/grpc_query_get_challenger_by_address.go
+++ x/poa/keeper/grpc_query_get_challenger_by_address.go
@@ -7,20 +7,19 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
+
 )
 
 func (k Keeper) GetChallengerByAddress(goCtx context.Context, req *types.QueryGetChallengerByAddressRequest) (*types.QueryGetChallengerByAddressResponse, error) {
 	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "[GetChallengerByAddress] failed. Invalid request.")
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "[GetChallengerByAddress] failed. Invalid request: %T.", req)
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	challenger, found := k.GetChallenger(ctx, req.Address)
 	if !found {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownAddress, "[GetChallengerByAddress][GetChallenger] failed. Couldn't find a challenger by the address.")
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownAddress, "[GetChallengerByAddress][GetChallenger] failed. Couldn't find a challenger by the this address: [ %T ], Make sure address is not empty OR invalid.", req.Address)
 	}
 
 	return &types.QueryGetChallengerByAddressResponse{Challenger: &challenger}, nil
