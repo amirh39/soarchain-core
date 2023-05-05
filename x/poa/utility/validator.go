ed4c8db41809e9b910605d570c9b7472cd9e93d3
golnar-m <golnar@soarrobotics.com>
2023-05-05 16:46:40 +0300

    feat(common): implement

--- x/poa/utility/validator.go
+++ x/poa/utility/validator.go
@@ -1,16 +1,24 @@
 package utility
 
-/** IsValidPublickey performs the validation of publickey */
-func ValidPubkey(input string) bool {
+/** ValidPublickey performs the validation of a publickey */
+func ValidPubkey(input string) bool { // TODO: Add validation of an object Pubkey
 	if len(input) == 0 || input == "" {
 		return false
 	}
 	return true
 }
 
-/** IsValidAddress performs the validation of address */
+/** ValidAddress performs the validation of an address */
 func ValidAddress(input string) bool {
 	if len(input) > 43 || len(input) < 43 || input == "" || input[0:4] != "soar" {
+		return false
+	}
+	return true
+}
+
+/** ValidString performs the validation of a string input */
+func ValidString(input string) bool {
+	if len(input) == 0 || input == "" {
 		return false
 	}
 	return true
