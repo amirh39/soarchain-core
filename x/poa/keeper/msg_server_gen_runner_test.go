2484d8fb82e7d989e838067bbd296f7028a59c07
golnar-m <golnar@soarrobotics.com>
2023-05-16 11:24:35 +0300

    test: add test file for testing verifying with valid/unvalid certificate

--- x/poa/keeper/msg_server_gen_runner_test.go
+++ x/poa/keeper/msg_server_gen_runner_test.go
@@ -8,13 +8,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	CERTIFICATE    = "-----BEGIN CERTIFICATE-----\nMIIB4TCCAYegAwIBAgIQTylBUpEkZd8CaYHSaLbBHzAKBggqhkjOPQQDAjBIMRwwGgYDVQQKDBNTb2FyIFJvYm90aWNzLCBJbmMuMSgwJgYDVQQDDB9Tb2FyIFJvYm90aWNzIFNlY3VyZSBFbGVtZW50IENBMB4XDTIzMDMzMDA2NTUxNVoXDTQ4MDMzMDA2NTUxNVowSDEcMBoGA1UECgwTU29hciBSb2JvdGljcywgSW5jLjEoMCYGA1UEAwwfU29hciBSb2JvdGljcyBTZWN1cmUgRWxlbWVudCBDQTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABLaCOXbFw/dRJXzXtvhSFWt92aUkdwRZPLmJWZFBFX55+XIDQsCGsQeMmU4pqsnXEB4/r842uYUinWsdzg4xUoqjUzBRMB0GA1UdDgQWBBRqxTRE6ZPuogp88TrNw1cwAYyPMjAfBgNVHSMEGDAWgBRqxTRE6ZPuogp88TrNw1cwAYyPMjAPBgNVHRMBAf8EBTADAQH/MAoGCCqGSM49BAMCA0gAMEUCIAHpI8Y6zPLaitMOGNAzzDAKb0PJw2r49vjzkFl5TIGPAiEArPJTReSmEnUJWFTcEIuYoWcRIBDI+GpianTVfX4uxNI=\n-----END CERTIFICATE-----"
-	MASTER_ACCOUNT = "soar1qt8myp9424ng6rv4fwf65u9a0ttfschw5j4sp8"
-	CREATOR        = "soar1w7a7eswnwvde6xcarjjlwuvf7g2p5q0pfr52vu"
-	FACTORY_CERT   = "-----BEGIN CERTIFICATE-----\nMIICBjCCAaygAwIBAgIQYuzJOUKNHYpHJFGtxphGmzAKBggqhkjOPQQDAjBIMRwwGgYDVQQKDBNTb2FyIFJvYm90aWNzLCBJbmMuMSgwJgYDVQQDDB9Tb2FyIFJvYm90aWNzIFNlY3VyZSBFbGVtZW50IENBMB4XDTIzMDQwNDEzMDAwMFoXDTMzMDQwNDEzMDAwMFowWjEcMBoGA1UECgwTU29hciBSb2JvdGljcywgSW5jLjE6MDgGA1UEAwwxU29hciBSb2JvdGljcyBTZWN1cmUgRWxlbWVudCBJbnQuIENBIDB4MDAwMTAyRkZGRjBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABIPvuGA6Q/Z1+lyExgKRM/v4bH77K3cGEKrfkQ/0ZQNhDbSfzKvrvDiKNPWYN1LhRgWcLzDguDkKisM8h1Jw2SGjZjBkMA4GA1UdDwEB/wQEAwIBhjASBgNVHRMBAf8ECDAGAQH/AgEAMB0GA1UdDgQWBBTL6JassDcrTsRXdHSf9H1Rg+83VzAfBgNVHSMEGDAWgBRqxTRE6ZPuogp88TrNw1cwAYyPMjAKBggqhkjOPQQDAgNIADBFAiAtY1bj66UiOLJaj8EMHdeCiMtu/TAwhx1ackbwYj6sOQIhAOx2lNKLmXqt1U5StSM3jZpI8w5dNStYigv8CcABJn0k\n-----END CERTIFICATE-----"
-)
-
 func Test_GenRunner(t *testing.T) {
 	msgServer, k, context, ctrl, bank := setupMsgServerClaimMotusRewards(t)
 	defer ctrl.Finish()
@@ -28,43 +21,48 @@ func Test_GenRunner(t *testing.T) {
 	}
 	k.SetMasterKey(ctx, item)
 
-	resp, err := msgServer.GenRunner(context, &types.MsgGenRunner{
+	res, err := msgServer.GenRunner(context, &types.MsgGenRunner{
 		Creator:      CREATOR,
-		RunnerPubKey: "3056301006072a8648ce3d020106052b8104000a034200044c1db1a1b1e19d6c423b1af88203ce79b6e4705d1dedaf65daeb0eedbe2c1fc6db010fa7f81443229d90181691df2e209be1c1278af42cc0f5ade03db549a795",
-		RunnerAddr:   "soar1w7a7eswnwvde6xcarjjlwuvf7g2p5q0pfr52vu",
-		RunnerStake:  "1000000000utmotus",
-		RunnerIp:     "104.248.142.45",
+		RunnerPubKey: PUBLICKEY,
+		RunnerAddr:   ADDRESS,
+		RunnerStake:  STAKE,
+		RunnerIp:     IP,
 		Certificate:  CERTIFICATE,
-		Signature:    "",
+		Signature:    SIGNATURE,
 	})
 
-	t.Log("response", resp)
+	t.Log("response", res)
 
 	require.NoError(t, err)
-	require.NotNil(t, resp)
+	require.NotNil(t, res)
 }
 
-/** Using not valid master certificate, response should raise proper error message*/
-// func Test_GenRunner_NotValidCertificate(t *testing.T) {
-// 	msgServer, k, context, ctrl, bank := setupMsgServerClaimMotusRewards(t)
-// 	defer ctrl.Finish()
+/** Using not valid certificate, response should raise proper error message*/
+func Test_GenRunner_NotValidCertificate(t *testing.T) {
+	msgServer, k, context, ctrl, bank := setupMsgServerClaimMotusRewards(t)
+	defer ctrl.Finish()
 
-// 	bank.ExpectAny(context)
+	bank.ExpectAny(context)
 
-// 	ctx := sdk.UnwrapSDKContext(context)
+	ctx := sdk.UnwrapSDKContext(context)
 
-// 	item := types.MasterKey{MasterCertificate: MASTER_ACCOUNT,
-// 		MasterAccount: MASTER_ACCOUNT,
-// 	}
-// 	k.SetMasterKey(ctx, item)
+	item := types.MasterKey{MasterCertificate: MASTER_ACCOUNT,
+		MasterAccount: MASTER_ACCOUNT,
+	}
+	k.SetMasterKey(ctx, item)
 
-// 	res, err := msgServer.RegisterFactoryKey(context, &types.MsgRegisterFactoryKey{
-// 		Creator:     CREATOR,
-// 		FactoryCert: FACTORY_CERT,
-// 	})
+	res, err := msgServer.GenRunner(context, &types.MsgGenRunner{
+		Creator:      CREATOR,
+		RunnerPubKey: PUBLICKEY,
+		RunnerAddr:   ADDRESS,
+		RunnerStake:  STAKE,
+		RunnerIp:     IP,
+		Certificate:  NOTVALIDCERTIFICATE,
+		Signature:    SIGNATURE,
+	})
 
-// 	t.Log("error message", err)
+	t.Log("error message", err)
 
-// 	require.Error(t, err)
-// 	require.Nil(t, res)
-// }
+	require.Error(t, err)
+	require.Nil(t, res)
+}
