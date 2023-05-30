d8fb06cf0f39ae37bedbd6c68af8ea2c181dd005
golnar-m <golnar@soarrobotics.com>
2023-05-30 18:42:55 +0300

    refactor(constants): cleanup and remove test params and test repeated codes into the new global test file named

--- x/poa/keeper/vrf_data_test.go
+++ x/poa/keeper/vrf_data_test.go
@@ -1,33 +1,17 @@
 package keeper_test
 
 import (
-	"strconv"
 	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/require"
 	keepertest "soarchain/testutil/keeper"
 	"soarchain/testutil/nullify"
-	"soarchain/x/poa/keeper"
-	"soarchain/x/poa/types"
+
+	"github.com/stretchr/testify/require"
 )
-
-// Prevent strconv unused error
-var _ = strconv.IntSize
-
-func createNVrfData(keeper *keeper.Keeper, ctx sdk.Context, n int) []types.VrfData {
-	items := make([]types.VrfData, n)
-	for i := range items {
-		items[i].Index = strconv.Itoa(i)
-
-		keeper.SetVrfData(ctx, items[i])
-	}
-	return items
-}
 
 func TestVrfDataGet(t *testing.T) {
 	keeper, ctx := keepertest.PoaKeeper(t)
-	items := createNVrfData(keeper, ctx, 10)
+	items := CreateNVrfData(keeper, ctx, 10)
 	for _, item := range items {
 		rst, found := keeper.GetVrfData(ctx,
 			item.Index,
@@ -41,7 +25,7 @@ func TestVrfDataGet(t *testing.T) {
 }
 func TestVrfDataRemove(t *testing.T) {
 	keeper, ctx := keepertest.PoaKeeper(t)
-	items := createNVrfData(keeper, ctx, 10)
+	items := CreateNVrfData(keeper, ctx, 10)
 	for _, item := range items {
 		keeper.RemoveVrfData(ctx,
 			item.Index,
@@ -55,7 +39,7 @@ func TestVrfDataRemove(t *testing.T) {
 
 func TestVrfDataGetAll(t *testing.T) {
 	keeper, ctx := keepertest.PoaKeeper(t)
-	items := createNVrfData(keeper, ctx, 10)
+	items := CreateNVrfData(keeper, ctx, 10)
 	require.ElementsMatch(t,
 		nullify.Fill(items),
 		nullify.Fill(keeper.GetAllVrfData(ctx)),
