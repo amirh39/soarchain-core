30b096898dcd1f484b245f7c624c0a7464262d03
golnar-m <golnar@soarrobotics.com>
2023-05-30 21:09:31 +0300

    refactor(constants): cleanup and remove test params and test repeated codes into the new global test file named

--- x/poa/keeper/v2vRewardCalculator_test.go
+++ x/poa/keeper/v2vRewardCalculator_test.go
@@ -2,32 +2,19 @@ package keeper_test
 
 import (
 	keepertest "soarchain/testutil/keeper"
-	"soarchain/x/poa/keeper"
-	"soarchain/x/poa/types"
-	"strconv"
+	"soarchain/x/poa/constants"
 	"testing"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
 )
-
-func createClient(keeper *keeper.Keeper, ctx sdk.Context, n int) []types.Client {
-	items := make([]types.Client, n)
-	for i := range items {
-		items[i].Index = strconv.Itoa(i)
-		items[i].RewardMultiplier = "4"
-		keeper.SetClient(ctx, items[i])
-	}
-	return items
-}
 
 func Test_V2VRewardCalculator(t *testing.T) {
 	keeper, ctx := keepertest.PoaKeeper(t)
 
-	clients := createClient(keeper, ctx, 10)
+	clients := CreateRewardCapClient(keeper, ctx, 10)
 	t.Log("clients", clients)
 
-	res, err := keeper.V2VRewardCalculator(ctx, 4, "v2v-rx")
+	res, err := keeper.V2VRewardCalculator(ctx, 4, constants.V2VRX)
 
 	t.Log("response", res)
 	t.Log("err", err)
@@ -39,10 +26,10 @@ func Test_V2VRewardCalculator(t *testing.T) {
 func Test_V2VRewardCalculator_NotValidClients(t *testing.T) {
 	keeper, ctx := keepertest.PoaKeeper(t)
 
-	clients := createNClient(keeper, ctx, 10)
+	clients := CreateNClient(keeper, ctx, 10)
 	t.Log("clients", clients)
 
-	res, err := keeper.V2NRewardCalculator(ctx, 4, "v2v-rx")
+	res, err := keeper.V2NRewardCalculator(ctx, 4, constants.V2VRX)
 
 	t.Log("response", res)
 	t.Log("err", err)
@@ -53,13 +40,13 @@ func Test_V2VRewardCalculator_NotValidClients(t *testing.T) {
 
 func Test_V2VRewardCalculator_NotValidRewardMultiplier(t *testing.T) {
 	keeper, ctx := keepertest.PoaKeeper(t)
-	runners := createClient(keeper, ctx, 0)
+	runners := CreateNRunner(keeper, ctx, 1)
 	t.Log("Runners", runners)
 
-	clients := createNClient(keeper, ctx, 10)
+	clients := CreateNClient(keeper, ctx, 10)
 	t.Log("clients", clients)
 
-	res, err := keeper.V2NRewardCalculator(ctx, 4, "runner")
+	res, err := keeper.V2NRewardCalculator(ctx, 4, constants.Runner)
 
 	t.Log("response", res)
 	t.Log("err", err)
