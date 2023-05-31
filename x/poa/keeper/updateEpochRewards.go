49fb2b5bd27bc8ea03fac3edd110098cf54d1996
Alperen-Dere <alperen-dere@hotmail.com>
2023-05-31 13:45:45 +0300

    feat: epoch data fixed according to challenges for all object types

--- x/poa/keeper/updateEpochRewards.go
+++ x/poa/keeper/updateEpochRewards.go
@@ -41,32 +41,50 @@ func (k Keeper) UpdateEpochRewards(ctx sdk.Context, clientType string, rewardToS
 		k.SetEpochData(ctx, newEpochData)
 
 	case "v2v-bx":
+		epochV2VBXCoin, err := sdk.ParseCoinNormalized(epochData.EpochV2VBX)
+		if err != nil {
+			return err
+		}
+		newEpochV2VBXCoin := epochV2VBXCoin.Sub(rewardToSet)
+		newEpochV2VBX := newEpochV2VBXCoin.String()
 		newEpochData := types.EpochData{
 			TotalEpochs: epochData.TotalEpochs,
 			EpochV2VRX:  epochData.EpochV2VRX,
-			EpochV2VBX:  rewardToSet.String(),
+			EpochV2VBX:  newEpochV2VBX,
 			EpochV2NBX:  epochData.EpochV2NBX,
 			EpochRunner: epochData.EpochRunner,
 		}
 		k.SetEpochData(ctx, newEpochData)
 
 	case "v2n-bx":
+		epochV2NBXCoin, err := sdk.ParseCoinNormalized(epochData.EpochV2NBX)
+		if err != nil {
+			return err
+		}
+		newEpochV2NBXCoin := epochV2NBXCoin.Sub(rewardToSet)
+		newEpochV2NBX := newEpochV2NBXCoin.String()
 		newEpochData := types.EpochData{
 			TotalEpochs: epochData.TotalEpochs,
 			EpochV2VRX:  epochData.EpochV2VRX,
 			EpochV2VBX:  epochData.EpochV2VBX,
-			EpochV2NBX:  rewardToSet.String(),
+			EpochV2NBX:  newEpochV2NBX,
 			EpochRunner: epochData.EpochRunner,
 		}
 		k.SetEpochData(ctx, newEpochData)
 
 	case "runner":
+		epochRunnerCoin, err := sdk.ParseCoinNormalized(epochData.EpochRunner)
+		if err != nil {
+			return err
+		}
+		newEpochRunnerCoin := epochRunnerCoin.Sub(rewardToSet)
+		newEpochRunner := newEpochRunnerCoin.String()
 		newEpochData := types.EpochData{
 			TotalEpochs: epochData.TotalEpochs,
 			EpochV2VRX:  epochData.EpochV2VRX,
 			EpochV2VBX:  epochData.EpochV2VBX,
 			EpochV2NBX:  epochData.EpochV2NBX,
-			EpochRunner: rewardToSet.String(),
+			EpochRunner: newEpochRunner,
 		}
 		k.SetEpochData(ctx, newEpochData)
 
