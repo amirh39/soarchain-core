3fc55d234c88ab437bc82ca6c22587ea5b5b17de
golnar-m <golnar@soarrobotics.com>
2023-06-05 08:17:55 +0300

    fix: avoid producing en error while running tests with correcting the variables

--- x/poa/utility/utilConstants/constants.go
+++ x/poa/utility/utilConstants/constants.go
@@ -14,8 +14,8 @@ const (
 	V2NBXInitialTokenPerYear              = 64283578560000
 	RunnerInitialTokenPerYear             = 21850083350000
 	MinutesPerYear                        = 525600
-	V2VReceiverInitialAnnual              = 37366809210000
-	V2VBroadcasterInitialAnnual           = 17416733110000
+	V2VReceiverInitialAnnual              = 373668092100
+	V2VBroadcasterInitialAnnual           = 174167331100
 	BlocksPerEpoch                        = 30
 	V2VRewardEmissionBlockPerYear         = 1
 	AddressLength                         = 43
