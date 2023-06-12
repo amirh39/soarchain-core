ae642dc8b163e82e4d668509e15794dea8e41e24
golnar-m <golnar@soarrobotics.com>
2023-06-12 22:32:36 +0300

    feat: add pubkey verification to make sure we objects(clienr, runner and challenger) are unique into the chain

--- x/poa/keeper/verifyPubkeys.go
+++ x/poa/keeper/verifyPubkeys.go
@@ -1,20 +1,17 @@
 package keeper
 
 import (
-
-	// "encoding/pem"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func IsUniquePubKey(k msgServer, ctx sdk.Context, address string, pubkey string) (isUnique bool) {
+func IsUniquePubKey(k msgServer, ctx sdk.Context, address string, pubkey string) (isUniquePubkey bool) {
 
 	_, isFoundWallet := k.GetMotusWallet(ctx, address)
 	_, isFoundAsClient := k.GetClient(ctx, pubkey)
 	_, isFoundAsRunner := k.GetRunnerUsingPubKey(ctx, pubkey)
 	_, isFoundAsChallenger := k.GetChallengerUsingPubKey(ctx, pubkey)
 	if isFoundWallet || isFoundAsChallenger || isFoundAsRunner || isFoundAsClient {
-		return false
+		return true
 	}
-	return true
+	return false
 }
