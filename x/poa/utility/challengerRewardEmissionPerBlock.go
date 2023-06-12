09c162b7a8ecc1cc11f1d433920c174377379981
Alperen-Dere <alperen-dere@hotmail.com>
2023-06-12 14:01:24 +0300

    feat: Challenger reward calculation for per epoch

--- x/poa/utility/challengerRewardEmissionPerBlock.go
+++ x/poa/utility/challengerRewardEmissionPerBlock.go
@@ -33,3 +33,28 @@ func ChallengerRewardEmissionPerBlock(ctx sdk.Context) (float64, error) {
 
 	return tokensPerBlock, nil
 }
+
+func ChallengerRewardEmissionPerEpoch(ctx sdk.Context) (float64, error) {
+	blocksPerMinute := utilConstants.BlocksPerMinute
+	currentBlockNumber := int(ctx.BlockHeight())
+	blocksPerEpoch := utilConstants.BlocksPerEpoch
+	initialTokensPerYear := utilConstants.InitialTokensPerYear
+
+	// Number of tokens issued per year by the total number of blocks produced per year
+	tokensPerBlock := initialTokensPerYear / (blocksPerMinute * minutesPerYear)
+
+	// Calculate the number of years that have passed since the start of the token issuance
+	yearsSinceStart := (currentBlockNumber) / (blocksPerMinute * minutesPerYear)
+
+	// Calculate the number of times the token issuance rate has been halved
+	halvings := (yearsSinceStart) / 3
+
+	// Update the token issuance rate for each halving that has occurred
+	for i := 0; i < halvings; i++ {
+		initialTokensPerYear /= 2
+		tokensPerBlock = initialTokensPerYear / (blocksPerMinute * minutesPerYear)
+	}
+
+	return float64(tokensPerBlock) * blocksPerEpoch, nil
+
+}
