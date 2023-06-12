5136fcda61ed4caca2325ba6cfc600af1be6b44d
golnar-m <golnar@soarrobotics.com>
2023-06-12 22:35:08 +0300

    feat: add address verification check to make sure we create unique clientobject

--- x/poa/keeper/msg_server_gen_client.go
+++ x/poa/keeper/msg_server_gen_client.go
@@ -49,9 +49,15 @@ func (k msgServer) GenClient(goCtx context.Context, msg *types.MsgGenClient) (*t
 		return nil, errCert
 	}
 
+	//check if the address is uniqe
+	isUniqueAddress := IsUniqueAddress(k, ctx, msg.Creator)
+	if isUniqueAddress {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "[GenClient][GetMotusWallet][GetChallengerUsingPubKey][GetRunnerUsingPubKey][GetClient] failed. Client with the address [ %T ] is already registered.", msg.Creator)
+	}
+
 	//check if the pubKey is uniqe, also check if msg.creator address have a motus wallet
-	isUnique := IsUniquePubKey(k, ctx, msg.Creator, pubKeyHex)
-	if !isUnique {
+	isUniquePubkey := IsUniquePubKey(k, ctx, msg.Creator, pubKeyHex)
+	if isUniquePubkey {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "[GenClient][GetMotusWallet][GetChallengerUsingPubKey][GetRunnerUsingPubKey][GetClient] failed. Client PubKey is not uniqe OR Client is already registered.")
 	}
 
