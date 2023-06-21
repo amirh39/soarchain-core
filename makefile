eb5848b656dac65141182a2e08c60ca280d3b245
Alperen-Dere <alperen-dere@hotmail.com>
2023-06-21 11:50:16 +0300

    feat: epoch data changed, rewardCalculator refactored

--- makefile
+++ makefile
@@ -18,13 +18,14 @@ keys:
 	soarchaind keys add apollo --keyring-backend test --algo secp256k1
 	soarchaind keys add client --recover --keyring-backend test --algo secp256k1
 	soarchaind keys add soarMasterAccount --recover --keyring-backend test --algo secp256k1
-
+	soarchaind keys add challenger --recover --keyring-backend test --algo secp256k1
+	soarchaind keys add runner --recover --keyring-backend test --algo secp256k1
 
 parameter_token_denomination:
-	cat ~/.soarchain/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="umotus"' > ~/.soarchain/config/tmp_genesis.json && mv ~/.soarchain/config/tmp_genesis.json ~/.soarchain/config/genesis.json
-	cat ~/.soarchain/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="umotus"' > ~/.soarchain/config/tmp_genesis.json && mv ~/.soarchain/config/tmp_genesis.json ~/.soarchain/config/genesis.json
-	cat ~/.soarchain/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="umotus"' > ~/.soarchain/config/tmp_genesis.json && mv ~/.soarchain/config/tmp_genesis.json ~/.soarchain/config/genesis.json
-	cat ~/.soarchain/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="umotus"' > ~/.soarchain/config/tmp_genesis.json && mv ~/.soarchain/config/tmp_genesis.json ~/.soarchain/config/genesis.json
+	cat ~/.soarchain/config/genesis.json | jq '.app_state["staking"]["params"]["bond_denom"]="utmotus"' > ~/.soarchain/config/tmp_genesis.json && mv ~/.soarchain/config/tmp_genesis.json ~/.soarchain/config/genesis.json
+	cat ~/.soarchain/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["denom"]="utmotus"' > ~/.soarchain/config/tmp_genesis.json && mv ~/.soarchain/config/tmp_genesis.json ~/.soarchain/config/genesis.json
+	cat ~/.soarchain/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="utmotus"' > ~/.soarchain/config/tmp_genesis.json && mv ~/.soarchain/config/tmp_genesis.json ~/.soarchain/config/genesis.json
+	cat ~/.soarchain/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="utmotus"' > ~/.soarchain/config/tmp_genesis.json && mv ~/.soarchain/config/tmp_genesis.json ~/.soarchain/config/genesis.json
 	sed -i 's/\("masterAccount": "\)[^"]*\(".*\)/\1'"$$(soarchaind keys show soarMasterAccount -a)"'\2/' ~/.soarchain/config/genesis.json
 
 	cat ~/.soarchain/config/genesis.json | jq '.app_state["poa"]["masterKey"]["masterCertificate"]="-----BEGIN CERTIFICATE-----\nMIIB4TCCAYegAwIBAgIQTylBUpEkZd8CaYHSaLbBHzAKBggqhkjOPQQDAjBIMRwwGgYDVQQKDBNTb2FyIFJvYm90aWNzLCBJbmMuMSgwJgYDVQQDDB9Tb2FyIFJvYm90aWNzIFNlY3VyZSBFbGVtZW50IENBMB4XDTIzMDMzMDA2NTUxNVoXDTQ4MDMzMDA2NTUxNVowSDEcMBoGA1UECgwTU29hciBSb2JvdGljcywgSW5jLjEoMCYGA1UEAwwfU29hciBSb2JvdGljcyBTZWN1cmUgRWxlbWVudCBDQTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABLaCOXbFw/dRJXzXtvhSFWt92aUkdwRZPLmJWZFBFX55+XIDQsCGsQeMmU4pqsnXEB4/r842uYUinWsdzg4xUoqjUzBRMB0GA1UdDgQWBBRqxTRE6ZPuogp88TrNw1cwAYyPMjAfBgNVHSMEGDAWgBRqxTRE6ZPuogp88TrNw1cwAYyPMjAPBgNVHRMBAf8EBTADAQH/MAoGCCqGSM49BAMCA0gAMEUCIAHpI8Y6zPLaitMOGNAzzDAKb0PJw2r49vjzkFl5TIGPAiEArPJTReSmEnUJWFTcEIuYoWcRIBDI+GpianTVfX4uxNI=\n-----END CERTIFICATE-----"' > ~/.soarchain/config/tmp_genesis.json && mv ~/.soarchain/config/tmp_genesis.json ~/.soarchain/config/genesis.json
@@ -34,21 +35,23 @@ parameter_voting_period:
 
 allocate_genesis_accounts:
 
-	soarchaind add-genesis-account soarMasterAccount 10000000000umotus --keyring-backend test
-	soarchaind add-genesis-account client 10000000000umotus --keyring-backend test
-	soarchaind add-genesis-account apollo 10000000000umotus --keyring-backend test
-	soarchaind add-genesis-account soar1qyhtcgw54973l3tz7fag27480q5qzt7cmsv9th 47500000umotus #airdrop
-	soarchaind add-genesis-account soar1wfly3s05fvtuqs7lpesr8nas6rydm96jh88m9v 100700000umotus #coummunityPool
-	soarchaind add-genesis-account soar1743qv44dgty0zv4t7vnnzdfhd7ftjfsmpreggg 77425000umotus #inverstorSeed
-	soarchaind add-genesis-account soar1c9k0cjhq0sma2mskl6re9mx93lxkavzzm6xdj4 36100000umotus #StrategicReserve0
-	soarchaind add-genesis-account soar1paaxlh6luwlxvv9smf935nj53hz0yk7wna2hu2 285000000umotus #team
-	soarchaind add-genesis-account soar1m6u0zxu4hkg4ycqawgrvmnlqhudqr32ydgal0m 36100000umotus #StrategicReserve1
-	soarchaind add-genesis-account soar1hmj5fccg6nuns2scxz0pvwqgyy44ntp2knvnfa 36100000umotus #StrategicReserve2
-	soarchaind add-genesis-account soar1s6n8jr600zhnefzdgv0d5z5mdhkx7au8k0glh0 36100000umotus #StrategicReserve3
-	soarchaind add-genesis-account soar1hcr2r7v54gdus8ud7u7vm0wggt9y7pp2qfxssc 36100000umotus #StrategicReserve4
+	soarchaind add-genesis-account soarMasterAccount 10000000000utmotus --keyring-backend test
+	soarchaind add-genesis-account client 10000000000utmotus --keyring-backend test
+	soarchaind add-genesis-account apollo 10000000000utmotus --keyring-backend test
+	soarchaind add-genesis-account challenger 10000000000utmotus --keyring-backend test
+	soarchaind add-genesis-account runner 10000000000utmotus --keyring-backend test
+	soarchaind add-genesis-account soar1qyhtcgw54973l3tz7fag27480q5qzt7cmsv9th 47500000utmotus #airdrop
+	soarchaind add-genesis-account soar1wfly3s05fvtuqs7lpesr8nas6rydm96jh88m9v 100700000utmotus #coummunityPool
+	soarchaind add-genesis-account soar1743qv44dgty0zv4t7vnnzdfhd7ftjfsmpreggg 77425000utmotus #inverstorSeed
+	soarchaind add-genesis-account soar1c9k0cjhq0sma2mskl6re9mx93lxkavzzm6xdj4 36100000utmotus #StrategicReserve0
+	soarchaind add-genesis-account soar1paaxlh6luwlxvv9smf935nj53hz0yk7wna2hu2 285000000utmotus #team
+	soarchaind add-genesis-account soar1m6u0zxu4hkg4ycqawgrvmnlqhudqr32ydgal0m 36100000utmotus #StrategicReserve1
+	soarchaind add-genesis-account soar1hmj5fccg6nuns2scxz0pvwqgyy44ntp2knvnfa 36100000utmotus #StrategicReserve2
+	soarchaind add-genesis-account soar1s6n8jr600zhnefzdgv0d5z5mdhkx7au8k0glh0 36100000utmotus #StrategicReserve3
+	soarchaind add-genesis-account soar1hcr2r7v54gdus8ud7u7vm0wggt9y7pp2qfxssc 36100000utmotus #StrategicReserve4
 
 sign_genesis_transaction:
-	soarchaind gentx apollo 2000000umotus --chain-id soarchaindevnet
+	soarchaind gentx apollo 2000000utmotus --chain-id soarchaindevnet
 
 collect_genesis_tx:
 	soarchaind collect-gentxs
@@ -57,7 +60,7 @@ validate_genesis:
 	soarchaind validate-genesis
 
 ifdef start
-	soarchaind start --log_level info --minimum-gas-prices=0.0001umotus
+	soarchaind start --log_level info --minimum-gas-prices=0.0001utmotus
 endif
 
 all: reset init config keys parameter_token_denomination allocate_genesis_accounts sign_genesis_transaction collect_genesis_tx validate_genesis
--- x/poa/types/epoch_data.pb.go
+++ x/poa/types/epoch_data.pb.go
@@ -23,12 +23,17 @@ var _ = math.Inf
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 type EpochData struct {
-	TotalEpochs     uint64 `protobuf:"varint,1,opt,name=totalEpochs,proto3" json:"totalEpochs,omitempty"`
-	EpochV2VRX      string `protobuf:"bytes,2,opt,name=epochV2VRX,proto3" json:"epochV2VRX,omitempty"`
-	EpochV2VBX      string `protobuf:"bytes,3,opt,name=epochV2VBX,proto3" json:"epochV2VBX,omitempty"`
-	EpochV2NBX      string `protobuf:"bytes,4,opt,name=epochV2NBX,proto3" json:"epochV2NBX,omitempty"`
-	EpochRunner     string `protobuf:"bytes,5,opt,name=epochRunner,proto3" json:"epochRunner,omitempty"`
-	TotalChallenges string `protobuf:"bytes,6,opt,name=totalChallenges,proto3" json:"totalChallenges,omitempty"`
+	TotalEpochs               uint64 `protobuf:"varint,1,opt,name=totalEpochs,proto3" json:"totalEpochs,omitempty"`
+	EpochV2VRX                string `protobuf:"bytes,2,opt,name=epochV2VRX,proto3" json:"epochV2VRX,omitempty"`
+	EpochV2VBX                string `protobuf:"bytes,3,opt,name=epochV2VBX,proto3" json:"epochV2VBX,omitempty"`
+	EpochV2NBX                string `protobuf:"bytes,4,opt,name=epochV2NBX,proto3" json:"epochV2NBX,omitempty"`
+	EpochRunner               string `protobuf:"bytes,5,opt,name=epochRunner,proto3" json:"epochRunner,omitempty"`
+	EpochChallenger           string `protobuf:"bytes,6,opt,name=epochChallenger,proto3" json:"epochChallenger,omitempty"`
+	V2VRXtotalChallenges      string `protobuf:"bytes,7,opt,name=V2VRXtotalChallenges,proto3" json:"V2VRXtotalChallenges,omitempty"`
+	V2VBXtotalChallenges      string `protobuf:"bytes,8,opt,name=V2VBXtotalChallenges,proto3" json:"V2VBXtotalChallenges,omitempty"`
+	V2NBXtotalChallenges      string `protobuf:"bytes,9,opt,name=V2NBXtotalChallenges,proto3" json:"V2NBXtotalChallenges,omitempty"`
+	RunnerTotalChallenges     string `protobuf:"bytes,10,opt,name=RunnerTotalChallenges,proto3" json:"RunnerTotalChallenges,omitempty"`
+	ChallengerTotalChallenges string `protobuf:"bytes,11,opt,name=ChallengerTotalChallenges,proto3" json:"ChallengerTotalChallenges,omitempty"`
 }
 
 func (m *EpochData) Reset()         { *m = EpochData{} }
@@ -99,9 +104,44 @@ func (m *EpochData) GetEpochRunner() string {
 	return ""
 }
 
-func (m *EpochData) GetTotalChallenges() string {
+func (m *EpochData) GetEpochChallenger() string {
 	if m != nil {
-		return m.TotalChallenges
+		return m.EpochChallenger
+	}
+	return ""
+}
+
+func (m *EpochData) GetV2VRXtotalChallenges() string {
+	if m != nil {
+		return m.V2VRXtotalChallenges
+	}
+	return ""
+}
+
+func (m *EpochData) GetV2VBXtotalChallenges() string {
+	if m != nil {
+		return m.V2VBXtotalChallenges
+	}
+	return ""
+}
+
+func (m *EpochData) GetV2NBXtotalChallenges() string {
+	if m != nil {
+		return m.V2NBXtotalChallenges
+	}
+	return ""
+}
+
+func (m *EpochData) GetRunnerTotalChallenges() string {
+	if m != nil {
+		return m.RunnerTotalChallenges
+	}
+	return ""
+}
+
+func (m *EpochData) GetChallengerTotalChallenges() string {
+	if m != nil {
+		return m.ChallengerTotalChallenges
 	}
 	return ""
 }
@@ -113,21 +153,25 @@ func init() {
 func init() { proto.RegisterFile("poa/epoch_data.proto", fileDescriptor_911419e87680592b) }
 
 var fileDescriptor_911419e87680592b = []byte{
-	// 221 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x29, 0xc8, 0x4f, 0xd4,
-	0x4f, 0x2d, 0xc8, 0x4f, 0xce, 0x88, 0x4f, 0x49, 0x2c, 0x49, 0xd4, 0x2b, 0x28, 0xca, 0x2f, 0xc9,
-	0x17, 0xe2, 0x2d, 0xce, 0x4f, 0x2c, 0x4a, 0xce, 0x48, 0xcc, 0xcc, 0xd3, 0x2b, 0xc8, 0x4f, 0x54,
-	0xba, 0xc9, 0xc8, 0xc5, 0xe9, 0x0a, 0x52, 0xe3, 0x92, 0x58, 0x92, 0x28, 0xa4, 0xc0, 0xc5, 0x5d,
-	0x92, 0x5f, 0x92, 0x98, 0x03, 0x16, 0x29, 0x96, 0x60, 0x54, 0x60, 0xd4, 0x60, 0x09, 0x42, 0x16,
-	0x12, 0x92, 0xe3, 0xe2, 0x02, 0x1b, 0x19, 0x66, 0x14, 0x16, 0x14, 0x21, 0xc1, 0xa4, 0xc0, 0xa8,
-	0xc1, 0x19, 0x84, 0x24, 0x82, 0x2c, 0xef, 0x14, 0x21, 0xc1, 0x8c, 0x2a, 0xef, 0x84, 0x2c, 0xef,
-	0xe7, 0x14, 0x21, 0xc1, 0x82, 0x22, 0xef, 0xe7, 0x14, 0x01, 0x72, 0x01, 0x98, 0x17, 0x54, 0x9a,
-	0x97, 0x97, 0x5a, 0x24, 0xc1, 0x0a, 0x56, 0x80, 0x2c, 0x24, 0xa4, 0xc1, 0xc5, 0x0f, 0x76, 0x90,
-	0x73, 0x46, 0x62, 0x4e, 0x4e, 0x6a, 0x5e, 0x7a, 0x6a, 0xb1, 0x04, 0x1b, 0x58, 0x15, 0xba, 0xb0,
-	0x93, 0xfe, 0x89, 0x47, 0x72, 0x8c, 0x17, 0x1e, 0xc9, 0x31, 0x3e, 0x78, 0x24, 0xc7, 0x38, 0xe1,
-	0xb1, 0x1c, 0xc3, 0x85, 0xc7, 0x72, 0x0c, 0x37, 0x1e, 0xcb, 0x31, 0x44, 0x89, 0xc2, 0x03, 0x41,
-	0xbf, 0x42, 0x1f, 0x14, 0x4c, 0x25, 0x95, 0x05, 0xa9, 0xc5, 0x49, 0x6c, 0xe0, 0x20, 0x32, 0x06,
-	0x04, 0x00, 0x00, 0xff, 0xff, 0x23, 0x8e, 0x12, 0xc6, 0x3a, 0x01, 0x00, 0x00,
+	// 285 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x74, 0x92, 0x31, 0x4b, 0xc3, 0x40,
+	0x14, 0xc7, 0x73, 0xb6, 0x56, 0xf3, 0x8a, 0x08, 0x47, 0x0b, 0xe7, 0x72, 0x04, 0xa7, 0x4c, 0x0d,
+	0x54, 0x47, 0xa7, 0x53, 0xd7, 0x0c, 0x41, 0x4a, 0x70, 0x91, 0x67, 0x0d, 0x46, 0x08, 0xb9, 0x90,
+	0x9c, 0xa0, 0xdf, 0xc2, 0x8f, 0xe5, 0xd8, 0xd1, 0x51, 0x92, 0xc9, 0x6f, 0x21, 0x79, 0x6a, 0x4c,
+	0x93, 0x74, 0xbc, 0xff, 0xef, 0xff, 0x3b, 0xde, 0x83, 0x07, 0xb3, 0x4c, 0xa3, 0x17, 0x65, 0x7a,
+	0x1d, 0xdf, 0x3d, 0xa0, 0xc1, 0x45, 0x96, 0x6b, 0xa3, 0xf9, 0x51, 0xa1, 0x31, 0x5f, 0xc7, 0xf8,
+	0x94, 0x2e, 0x32, 0x8d, 0xa7, 0x5f, 0x23, 0xb0, 0xaf, 0xeb, 0xce, 0x15, 0x1a, 0xe4, 0x0e, 0x4c,
+	0x8d, 0x36, 0x98, 0x50, 0x52, 0x08, 0xe6, 0x30, 0x77, 0x1c, 0xb4, 0x23, 0x2e, 0x01, 0xe8, 0xcb,
+	0xd5, 0x72, 0x15, 0x84, 0x62, 0xcf, 0x61, 0xae, 0x1d, 0xb4, 0x92, 0x36, 0x57, 0xa1, 0x18, 0x6d,
+	0x73, 0xd5, 0xe6, 0xbe, 0x0a, 0xc5, 0x78, 0x8b, 0xfb, 0x2a, 0xac, 0x27, 0xa0, 0x57, 0xf0, 0x9c,
+	0xa6, 0x51, 0x2e, 0xf6, 0xa9, 0xd0, 0x8e, 0xb8, 0x0b, 0xc7, 0xf4, 0xbc, 0x8c, 0x31, 0x49, 0xa2,
+	0xf4, 0x31, 0xca, 0xc5, 0x84, 0x5a, 0xdd, 0x98, 0x2f, 0x61, 0x46, 0x43, 0xd1, 0xfc, 0x4d, 0x5e,
+	0x88, 0x03, 0xaa, 0x0f, 0xb2, 0x5f, 0x47, 0xf5, 0x9c, 0xc3, 0xc6, 0x51, 0xc3, 0x8e, 0xdf, 0x77,
+	0xec, 0x3f, 0xa7, 0xcf, 0xf8, 0x39, 0xcc, 0x7f, 0xf6, 0xb9, 0xe9, 0x48, 0x40, 0xd2, 0x30, 0xe4,
+	0x17, 0x70, 0xf2, 0xbf, 0x5f, 0xd7, 0x9c, 0x92, 0xb9, 0xbb, 0xa0, 0xbc, 0xf7, 0x52, 0xb2, 0x4d,
+	0x29, 0xd9, 0x67, 0x29, 0xd9, 0x5b, 0x25, 0xad, 0x4d, 0x25, 0xad, 0x8f, 0x4a, 0x5a, 0xb7, 0xf3,
+	0xe6, 0x28, 0xbc, 0x17, 0xaf, 0x3e, 0x1b, 0xf3, 0x9a, 0x45, 0xc5, 0xfd, 0x84, 0x4e, 0xe6, 0xec,
+	0x3b, 0x00, 0x00, 0xff, 0xff, 0x56, 0xce, 0xcd, 0x3f, 0x4a, 0x02, 0x00, 0x00,
 }
 
 func (m *EpochData) Marshal() (dAtA []byte, err error) {
@@ -150,10 +194,45 @@ func (m *EpochData) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.TotalChallenges) > 0 {
-		i -= len(m.TotalChallenges)
-		copy(dAtA[i:], m.TotalChallenges)
-		i = encodeVarintEpochData(dAtA, i, uint64(len(m.TotalChallenges)))
+	if len(m.ChallengerTotalChallenges) > 0 {
+		i -= len(m.ChallengerTotalChallenges)
+		copy(dAtA[i:], m.ChallengerTotalChallenges)
+		i = encodeVarintEpochData(dAtA, i, uint64(len(m.ChallengerTotalChallenges)))
+		i--
+		dAtA[i] = 0x5a
+	}
+	if len(m.RunnerTotalChallenges) > 0 {
+		i -= len(m.RunnerTotalChallenges)
+		copy(dAtA[i:], m.RunnerTotalChallenges)
+		i = encodeVarintEpochData(dAtA, i, uint64(len(m.RunnerTotalChallenges)))
+		i--
+		dAtA[i] = 0x52
+	}
+	if len(m.V2NBXtotalChallenges) > 0 {
+		i -= len(m.V2NBXtotalChallenges)
+		copy(dAtA[i:], m.V2NBXtotalChallenges)
+		i = encodeVarintEpochData(dAtA, i, uint64(len(m.V2NBXtotalChallenges)))
+		i--
+		dAtA[i] = 0x4a
+	}
+	if len(m.V2VBXtotalChallenges) > 0 {
+		i -= len(m.V2VBXtotalChallenges)
+		copy(dAtA[i:], m.V2VBXtotalChallenges)
+		i = encodeVarintEpochData(dAtA, i, uint64(len(m.V2VBXtotalChallenges)))
+		i--
+		dAtA[i] = 0x42
+	}
+	if len(m.V2VRXtotalChallenges) > 0 {
+		i -= len(m.V2VRXtotalChallenges)
+		copy(dAtA[i:], m.V2VRXtotalChallenges)
+		i = encodeVarintEpochData(dAtA, i, uint64(len(m.V2VRXtotalChallenges)))
+		i--
+		dAtA[i] = 0x3a
+	}
+	if len(m.EpochChallenger) > 0 {
+		i -= len(m.EpochChallenger)
+		copy(dAtA[i:], m.EpochChallenger)
+		i = encodeVarintEpochData(dAtA, i, uint64(len(m.EpochChallenger)))
 		i--
 		dAtA[i] = 0x32
 	}
@@ -229,7 +308,27 @@ func (m *EpochData) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovEpochData(uint64(l))
 	}
-	l = len(m.TotalChallenges)
+	l = len(m.EpochChallenger)
+	if l > 0 {
+		n += 1 + l + sovEpochData(uint64(l))
+	}
+	l = len(m.V2VRXtotalChallenges)
+	if l > 0 {
+		n += 1 + l + sovEpochData(uint64(l))
+	}
+	l = len(m.V2VBXtotalChallenges)
+	if l > 0 {
+		n += 1 + l + sovEpochData(uint64(l))
+	}
+	l = len(m.V2NBXtotalChallenges)
+	if l > 0 {
+		n += 1 + l + sovEpochData(uint64(l))
+	}
+	l = len(m.RunnerTotalChallenges)
+	if l > 0 {
+		n += 1 + l + sovEpochData(uint64(l))
+	}
+	l = len(m.ChallengerTotalChallenges)
 	if l > 0 {
 		n += 1 + l + sovEpochData(uint64(l))
 	}
@@ -420,7 +519,7 @@ func (m *EpochData) Unmarshal(dAtA []byte) error {
 			iNdEx = postIndex
 		case 6:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field TotalChallenges", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field EpochChallenger", wireType)
 			}
 			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
@@ -448,7 +547,167 @@ func (m *EpochData) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.TotalChallenges = string(dAtA[iNdEx:postIndex])
+			m.EpochChallenger = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 7:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field V2VRXtotalChallenges", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEpochData
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.V2VRXtotalChallenges = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 8:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field V2VBXtotalChallenges", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEpochData
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.V2VBXtotalChallenges = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 9:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field V2NBXtotalChallenges", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEpochData
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.V2NBXtotalChallenges = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 10:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field RunnerTotalChallenges", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEpochData
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.RunnerTotalChallenges = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 11:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field ChallengerTotalChallenges", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowEpochData
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthEpochData
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.ChallengerTotalChallenges = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
