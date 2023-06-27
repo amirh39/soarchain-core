c62bd16afc717e17632401a213defc23d163fbb5
golnar-m <golnar@soarrobotics.com>
2023-06-27 09:10:49 +0300

    feat: add , ,  as  new error messages

--- x/poa/errors/common_messages.go
+++ x/poa/errors/common_messages.go
@@ -1,5 +1,8 @@
 package errors
 
 var (
-	ConverToString = "[Type Error] Couldn't convert from Int to String."
+	ConverToString    = "[Type Error] Couldn't convert from Int to String."
+	ParseUint         = "Couldn't parse data."
+	ErrInvalidType    = "Couln't find valid type"
+	ErrInvalidAddress = "[AccAddressFromBech32] failed. Empty address string is not allowed."
 )
