a62bd5a329468f5da86540bd5c574963228b9ba9
golnar-m <golnar@soarrobotics.com>
2023-07-21 12:56:20 +0300

    test(new epoch module): add test file for test of  fetching epoch data to new epoch module

---
+++ x/epoch/keeper/grpc_query_epoch_data.go
@@ -0,0 +1,25 @@
+package keeper
+
+import (
+	"context"
+
+	"soarchain/x/epoch/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (k Keeper) EpochData(c context.Context, req *types.QueryGetEpochDataRequest) (*types.QueryGetEpochDataResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "[EpochData] failed. Invalid request.")
+	}
+	ctx := sdk.UnwrapSDKContext(c)
+
+	val, found := k.GetEpochData(ctx)
+	if !found {
+		return nil, status.Error(codes.NotFound, "[EpochData][GetEpochData] failed. Couldn't find data for an epoch.")
+	}
+
+	return &types.QueryGetEpochDataResponse{EpochData: val}, nil
+}
