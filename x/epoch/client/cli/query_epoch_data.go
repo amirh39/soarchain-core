912cafbb722e7e803a7840b9dc85338d28e04bfa
golnar-m <golnar@soarrobotics.com>
2023-07-21 12:51:29 +0300

    feat(new epoch module): add file to query epoch data for the new epoch module

---
+++ x/epoch/client/cli/query_epoch_data.go
@@ -0,0 +1,37 @@
+package cli
+
+import (
+	"context"
+
+	"soarchain/x/epoch/types"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+)
+
+func CmdShowEpochData() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-epoch-data",
+		Short: "shows epochData",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryGetEpochDataRequest{}
+
+			res, err := queryClient.EpochData(context.Background(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
