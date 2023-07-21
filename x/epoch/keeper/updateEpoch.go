f7a520c788a6b2ea26c8df3636f24ae40e8511c2
golnar-m <golnar@soarrobotics.com>
2023-07-21 13:01:26 +0300

    test(new epoch module): remove updating epoch data from poa module to the new epoch module

---
+++ x/epoch/keeper/updateEpoch.go
@@ -0,0 +1,31 @@
+package keeper
+
+import (
+	params "soarchain/app/params"
+	"soarchain/x/epoch/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k Keeper) UpdateEpoch(ctx sdk.Context) {
+	epochData, _ := k.GetEpochData(ctx)
+	epochCnt := epochData.TotalEpochs
+	newEpochCnt := epochCnt + 1
+
+	newEpochData := types.EpochData{
+		TotalEpochs:               newEpochCnt,
+		EpochV2VRX:                sdk.NewCoin(params.BondDenom, sdk.ZeroInt()).String(),
+		EpochV2VBX:                sdk.NewCoin(params.BondDenom, sdk.ZeroInt()).String(),
+		EpochV2NBX:                sdk.NewCoin(params.BondDenom, sdk.ZeroInt()).String(),
+		EpochRunner:               sdk.NewCoin(params.BondDenom, sdk.ZeroInt()).String(),
+		EpochChallenger:           sdk.NewCoin(params.BondDenom, sdk.ZeroInt()).String(),
+		V2VRXtotalChallenges:      0,
+		V2VBXtotalChallenges:      0,
+		V2NBXtotalChallenges:      0,
+		RunnerTotalChallenges:     0,
+		ChallengerTotalChallenges: 0,
+	}
+
+	k.SetEpochData(ctx, newEpochData)
+
+}
