244071fa379d39fa464ffd62c485332772a9bc3f
golnar-m <golnar@soarrobotics.com>
2023-07-21 12:55:06 +0300

    feat(new epoch module): add data file for epoch data activities regarding kvstore to new epoch module

---
+++ x/epoch/keeper/epoch_data.go
@@ -0,0 +1,34 @@
+package keeper
+
+import (
+	"soarchain/x/epoch/types"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// SetEpochData set epochData in the store
+func (k Keeper) SetEpochData(ctx sdk.Context, epochData types.EpochData) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.EpochDataKey))
+	b := k.cdc.MustMarshal(&epochData)
+	store.Set([]byte{0}, b)
+}
+
+// GetEpochData returns epochData
+func (k Keeper) GetEpochData(ctx sdk.Context) (val types.EpochData, found bool) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.EpochDataKey))
+
+	b := store.Get([]byte{0})
+	if b == nil {
+		return val, false
+	}
+
+	k.cdc.MustUnmarshal(b, &val)
+	return val, true
+}
+
+// RemoveEpochData removes epochData from the store
+func (k Keeper) RemoveEpochData(ctx sdk.Context) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.EpochDataKey))
+	store.Delete([]byte{0})
+}
