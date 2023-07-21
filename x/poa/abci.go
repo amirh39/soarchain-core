3dcca2bc77784179fa39fca6177081c76cc630b8
golnar-m <golnar@soarrobotics.com>
2023-07-21 13:07:47 +0300

    feat(new epoch module): update poa abci file to support new epoch module

--- x/poa/abci.go
+++ x/poa/abci.go
@@ -1,6 +1,7 @@
 package poa
 
 import (
+	"log"
 	"time"
 
 	"github.com/cosmos/cosmos-sdk/telemetry"
@@ -12,17 +13,43 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
-func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k keeper.Keeper) {
+func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k AppModule) {
 	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyBeginBlocker)
+
+	logger := k.keeper.Logger(ctx)
+
+	log.Println("############## Begin Blocker Started ##############")
 
 	// BeginBlocker for the PoA module. It checks if a new epoch has started and if so,
 	// it mints coins from the banking module to the PoA module according to total rewards earned during the epoch.
 
-	k.MintRewardCoins(ctx)
+	if logger != nil {
+		logger.Info("Mint Reward Coins started.", "path", "BeginBlocker")
+	}
+
+	k.keeper.MintRewardCoins(ctx)
+
+	if logger != nil {
+		logger.Info("Mint Reward Coins successfully done.", "path", "BeginBlocker")
+	}
+
 	// check if a new epoch has started
 	if (ctx.BlockHeight()%30 == 0) && (ctx.BlockHeight() != 0) {
-		k.UpdateEpoch(ctx)
+		if logger != nil {
+			logger.Info("Update epoch started.", "path", "BeginBlocker")
+		}
+
+		item, found := k.epochKeeper.GetEpochData(ctx)
+
+		if logger != nil {
+			logger.Info("Update epoch  successfully done.", "path", "BeginBlocker", "epoch data", item, "found", found)
+		}
+
+		k.epochKeeper.UpdateEpoch(ctx)
+
 	}
+
+	log.Println("############## End of Begin Blocker ##############")
 
 }
 
