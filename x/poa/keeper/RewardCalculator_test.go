d3f4d7f20035e8c71988a4218c066170c4a6457e
Alperen-Dere <alperen-dere@hotmail.com>
2023-08-06 22:49:36 +0300

    test: RewardCalculator unit-test updated

--- x/poa/keeper/RewardCalculator_test.go
+++ x/poa/keeper/RewardCalculator_test.go
@@ -1,44 +1,37 @@
 package keeper_test
 
-// func TestCalculateRewards(t *testing.T) {
-// 	keeper, _ := keepertest.PoaKeeper(t)
+import (
+	"math/big"
+	keepertest "soarchain/testutil/keeper"
+	"testing"
+)
 
-// 	// Set up test data
-// 	totalAmount := sdk.NewDec(1000)
-// 	scores := []sdk.Dec{
-// 		sdk.NewDec(50),
-// 		sdk.NewDec(30),
-// 		sdk.NewDec(20),
-// 	}
+func TestCalculateRewards(t *testing.T) {
+	keeper, _ := keepertest.PoaKeeper(t)
 
-// 	// Execute the function to be tested
-// 	rewards := keeper.CalculateRewards(totalAmount, scores)
+	totalAmount := big.NewInt(1000)             // Example total amount
+	scores := []float64{30.0, 40.0, 20.0, 10.0} // Example scores
 
-// 	// Validate the results
+	rewards := keeper.CalculateRewards(totalAmount, scores)
 
-// 	// Check if the number of rewards matches the number of scores
-// 	require.Len(t, rewards, len(scores), "Incorrect number of rewards")
+	// Check if the number of rewards matches the number of scores
+	if len(rewards) != len(scores) {
+		t.Errorf("Expected %d rewards, but got %d", len(scores), len(rewards))
+	}
 
-// 	// Check if the sum of rewards equals the total amount
-// 	sumRewards := sdk.ZeroDec()
-// 	for _, reward := range rewards {
-// 		sumRewards = sumRewards.Add(reward)
-// 	}
-// 	require.True(t, totalAmount.Equal(sumRewards), "Sum of rewards does not match totalAmount")
+	// Calculate total reward and check if it matches the total amount
+	totalReward := big.NewInt(0)
+	for _, reward := range rewards {
+		totalReward.Add(totalReward, reward)
+	}
+	if totalReward.Cmp(totalAmount) != 0 {
+		t.Errorf("Total reward should be equal to the total amount")
+	}
 
-// 	// Check if each reward is correctly calculated
-// 	for i, score := range scores {
-// 		expectedReward := totalAmount.Mul(score.Quo(calculateTotalScore(scores)))
-// 		require.True(t, expectedReward.Equal(rewards[i]), "Incorrect reward calculation for score %s", score.String())
-// 		t.Logf("Score: %s, Expected Reward: %s, Actual Reward: %s", score.String(), expectedReward.String(), rewards[i].String())
-// 	}
-// }
-
-// // Helper function to calculate the total score for test validation
-// func calculateTotalScore(scores []sdk.Dec) sdk.Dec {
-// 	totalScore := sdk.ZeroDec()
-// 	for _, score := range scores {
-// 		totalScore = totalScore.Add(score)
-// 	}
-// 	return totalScore
-// }
+	// Check if each reward is a non-negative value
+	for _, reward := range rewards {
+		if reward.Cmp(big.NewInt(0)) < 0 {
+			t.Errorf("Reward should be non-negative")
+		}
+	}
+}
