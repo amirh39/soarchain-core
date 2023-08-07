3b93928a5990423a43cb7d5d2d737b82a400b66c
Alperen-Dere <alperen-dere@hotmail.com>
2023-08-08 01:23:57 +0300

    test: adaptiveHalving test refactored

--- x/poa/utility/adaptiveHalving_test.go
+++ x/poa/utility/adaptiveHalving_test.go
@@ -28,26 +28,3 @@ func TestCalculateMintedPerChallenge(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, float64(4.4), mintedPerChallenge)
 }
-
-// since the error message is wrapping in a Cosmos SDK error, so function should return a standard Go error for the division by zero scenario to test it properly.
-// func TestCalculateMintedPerChallenge_DivisionByZero(t *testing.T) {
-// 	prevMintedPerChallenge := 10.0
-// 	totalChallengesPrevDay := 0
-// 	A, B, C := 11.0, 10.0, 10.0
-
-// 	_, err := CalculateMintedPerChallenge(prevMintedPerChallenge, totalChallengesPrevDay, A, B, C)
-// 	assert.EqualError(t, err, "division by zero in CalculateMintedPerChallenge")
-
-// }
-
-// func TestCalculateMintedPerChallenge_NegativeResult(t *testing.T) {
-// 	prevMintedPerChallenge := 10.0
-// 	totalChallengesPrevDay := 20
-// 	A, B, C := 11.0, 10.0, 10.0
-
-// 	mintedPerChallenge, err := CalculateMintedPerChallenge(prevMintedPerChallenge, totalChallengesPrevDay, A, B, C)
-
-// 	assert.Error(t, err)
-// 	assert.EqualError(t, err, "mintedPerChallenge should not be negative")
-// 	assert.Equal(t, float64(0), mintedPerChallenge)
-// }
