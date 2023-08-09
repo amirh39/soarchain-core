864ecc646f8bfaa98056b598d1df649312c022c3
golnar-m <golnar@soarrobotics.com>
2023-08-09 13:34:14 +0300

    fix(inconsistencey variables): replace TypeMsgGenrunner to the correct one TypeMsgGenRunner

--- x/poa/types/message_gen_runner.go
+++ x/poa/types/message_gen_runner.go
@@ -5,15 +5,15 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-const TypeMsgGenrunner = "gen_runner"
+const TypeMsgGenRunner = "gen_runner"
 
 var _ sdk.Msg = &MsgGenRunner{}
 
-func NewMsgGenRunner(creator string, runnerStake string, runnerIpaddress string, certificate string, signature string) *MsgGenRunner {
+func NewMsgGenRunner(creator string, runnerStake string, runnerIpAddress string, certificate string, signature string) *MsgGenRunner {
 	return &MsgGenRunner{
 		Creator:     creator,
 		RunnerStake: runnerStake,
-		RunnerIp:    runnerIpaddress,
+		RunnerIp:    runnerIpAddress,
 		Certificate: certificate,
 		Signature:   signature,
 	}
@@ -24,17 +24,16 @@ func (msg *MsgGenRunner) Route() string {
 }
 
 func (msg *MsgGenRunner) Type() string {
-	return TypeMsgGenrunner
+	return TypeMsgGenRunner
 }
 
 func (msg *MsgGenRunner) GetSigners() []sdk.AccAddress {
 	creator, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
-		sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "[AccAddressFromBech32] failed. Empty address string is not allowed.")
+		sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "[NewMsgGenRunner][AccAddressFromBech32] failed. Empty address string is not allowed.")
 		return nil
 	}
 	return []sdk.AccAddress{creator}
-
 }
 
 func (msg *MsgGenRunner) GetSignBytes() []byte {
@@ -45,7 +44,7 @@ func (msg *MsgGenRunner) GetSignBytes() []byte {
 func (msg *MsgGenRunner) ValidateBasic() error {
 	_, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "invalid creator address (%s)", err)
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "[NewMsgGenRunner][ValidateBasic] failed. Invalid creator address (%s)", err)
 	}
 	return nil
 }
