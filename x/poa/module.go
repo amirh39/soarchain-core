bd12abcb047fd285b742e879926c9dd9b5555b3d
golnar-m <golnar@soarrobotics.com>
2023-08-09 13:22:04 +0300

    fix(inconsistencey variables): refactor error message to handle it with the proper message

--- x/poa/module.go
+++ x/poa/module.go
@@ -67,7 +67,7 @@ func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncodingConfig, bz json.RawMessage) error {
 	var genState types.GenesisState
 	if err := cdc.UnmarshalJSON(bz, &genState); err != nil {
-		return fmt.Errorf("failed to unmarshal %s genesis state: %w", types.ModuleName, err)
+		return fmt.Errorf("[ValidateGenesis][UnmarshalJSON] failed. Unable to unmarshal %s genesis state: %w", types.ModuleName, err)
 	}
 	return genState.Validate()
 }
