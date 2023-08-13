8e2dff8f8d64c49a30c46270741e56390efd1af2
Alperen-Dere <alperen-dere@hotmail.com>
2023-08-14 00:29:14 +0300

    test: CreateEpochData test values changed

--- x/epoch/keeper/testConstants_test.go
+++ x/epoch/keeper/testConstants_test.go
@@ -10,10 +10,10 @@ import (
 func CreateEpochData(keeper *keeper.Keeper, ctx sdk.Context) types.EpochData {
 	item := types.EpochData{
 		TotalEpochs:               30,
-		EpochV2VRX:                "2utmotus",
-		EpochV2VBX:                "3utmotus",
-		EpochV2NBX:                "4utmotus",
-		EpochRunner:               "5utmotus",
+		EpochV2VRX:                "2udmotus",
+		EpochV2VBX:                "3udmotus",
+		EpochV2NBX:                "4udmotus",
+		EpochRunner:               "5udmotus",
 		EpochChallenger:           "6",
 		V2VRXTotalChallenges:      7,
 		V2VBXTotalChallenges:      8,
