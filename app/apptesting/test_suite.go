f855e62bd1736d4205194ce6e6832fe959597900
Alperen-Dere <alperen-dere@hotmail.com>
2023-08-17 10:41:29 +0300

    feat: KeeperTestSuite implemented in app level

---
+++ app/apptesting/test_suite.go
@@ -0,0 +1,186 @@
+package apptesting
+
+import (
+	"time"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/store/rootmulti"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/suite"
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+	"github.com/tendermint/tendermint/libs/log"
+
+	param "soarchain/app/params"
+
+	"soarchain/app"
+	"soarchain/x/epoch/types"
+	poatypes "soarchain/x/poa/types"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	slashingtypes "github.com/cosmos/cosmos-sdk/x/slashing/types"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+	tmtypes "github.com/tendermint/tendermint/proto/tendermint/types"
+	dbm "github.com/tendermint/tm-db"
+)
+
+type KeeperTestSuite struct {
+	suite.Suite
+	Ctx         sdk.Context
+	App         *app.SoarchainApp
+	QueryHelper *baseapp.QueryServiceTestHelper
+	TestAccs    []sdk.AccAddress
+}
+
+// Setup sets up basic environment for suite (App, Ctx, and test accounts)
+func (s *KeeperTestSuite) Setup() {
+	s.App = app.Setup(false)
+	s.Ctx = s.App.BaseApp.NewContext(false, tmtypes.Header{Height: 1, ChainID: "soarchain", Time: time.Now().UTC()})
+	s.QueryHelper = &baseapp.QueryServiceTestHelper{
+		GRPCQueryRouter: s.App.GRPCQueryRouter(),
+		Ctx:             s.Ctx,
+	}
+
+	s.SetEpoch()
+	s.TestAccs = CreateRandomAccounts(3)
+}
+
+func (s *KeeperTestSuite) SetEpoch() {
+	epochsKeeper := s.App.EpochKeeper
+	epochData := types.EpochData{
+		TotalEpochs:                   1,
+		EpochV2VRX:                    "0udmotus",
+		EpochV2VBX:                    "0udmotus",
+		EpochV2NBX:                    "0udmotus",
+		EpochRunner:                   "0udmotus",
+		EpochChallenger:               "0udmotus",
+		V2VRXTotalChallenges:          0,
+		V2VBXTotalChallenges:          0,
+		V2NBXTotalChallenges:          0,
+		ChallengerTotalChallenges:     0,
+		RunnerTotalChallenges:         0,
+		TotalChallengesPrevDay:        0,
+		InitialPerChallengeValue:      12.0,
+		V2VRXLastBlockChallenges:      100,
+		V2VRXPerChallengeValue:        2,
+		V2VBXLastBlockChallenges:      150,
+		V2VBXPerChallengeValue:        3,
+		V2NBXLastBlockChallenges:      200,
+		V2NBXPerChallengeValue:        4,
+		RunnerLastBlockChallenges:     250,
+		RunnerPerChallengeValue:       5,
+		ChallengerLastBlockChallenges: 300,
+		ChallengerPerChallengeValue:   6,
+	}
+	epochsKeeper.SetEpochData(s.Ctx, epochData)
+
+}
+
+// CreateRandomAccounts is a function return a list of randomly generated AccAddresses
+func CreateRandomAccounts(numAccts int) []sdk.AccAddress {
+	testAddrs := make([]sdk.AccAddress, numAccts)
+	for i := 0; i < numAccts; i++ {
+		pk := ed25519.GenPrivKey().PubKey()
+		testAddrs[i] = sdk.AccAddress(pk.Address())
+	}
+
+	return testAddrs
+}
+
+func (s *KeeperTestSuite) FundAcc(acc sdk.AccAddress, amounts sdk.Coins) {
+
+	ctx := s.App.NewContext(true, tmproto.Header{Height: s.App.LastBlockHeight()})
+
+	err := simapp.FundAccount(s.App.BankKeeper, ctx, acc, amounts)
+	s.Require().NoError(err)
+}
+
+func (s *KeeperTestSuite) FundModuleAcc(moduleName string, amounts sdk.Coins) {
+	err := simapp.FundModuleAccount(s.App.BankKeeper, s.Ctx, moduleName, amounts)
+	s.Require().NoError(err)
+}
+
+func (s *KeeperTestSuite) MintCoins(coins sdk.Coins) {
+	err := s.App.BankKeeper.MintCoins(s.Ctx, poatypes.ModuleName, coins)
+	s.Require().NoError(err)
+}
+
+// CreateTestContext creates a test context.
+func (s *KeeperTestSuite) CreateTestContext() sdk.Context {
+	ctx, _ := s.CreateTestContextWithMultiStore()
+	return ctx
+}
+
+// CreateTestContextWithMultiStore creates a test context and returns it together with multi store.
+func (s *KeeperTestSuite) CreateTestContextWithMultiStore() (sdk.Context, sdk.CommitMultiStore) {
+	db := dbm.NewMemDB()
+	logger := log.NewNopLogger()
+
+	ms := rootmulti.NewStore(db)
+
+	return sdk.NewContext(ms, tmtypes.Header{}, false, logger), ms
+}
+
+// CreateTestContext creates a test context.
+func (s *KeeperTestSuite) Commit() {
+	oldHeight := s.Ctx.BlockHeight()
+	oldHeader := s.Ctx.BlockHeader()
+	s.App.Commit()
+	newHeader := tmtypes.Header{Height: oldHeight + 1, ChainID: oldHeader.ChainID, Time: oldHeader.Time.Add(time.Second)}
+	s.App.BeginBlock(abci.RequestBeginBlock{Header: newHeader})
+	s.Ctx = s.App.GetBaseApp().NewContext(false, newHeader)
+}
+
+// SetupValidator sets up a validator and returns the ValAddress.
+func (s *KeeperTestSuite) SetupValidatorInvarient(bondStatus stakingtypes.BondStatus) sdk.ValAddress {
+
+	valPubKey := secp256k1.GenPrivKey().PubKey()
+	valAddress := sdk.ValAddress(valPubKey.Address())
+	bondDenom := param.BondDenom
+	selfBond := sdk.NewCoins(sdk.Coin{Amount: sdk.NewInt(100), Denom: bondDenom})
+
+	s.FundAcc(sdk.AccAddress(valAddress), selfBond)
+
+	stakingHandler := staking.NewHandler(s.App.StakingKeeper)
+	stakingCoin := sdk.NewCoin(sdk.DefaultBondDenom, selfBond[0].Amount)
+	ZeroCommission := stakingtypes.NewCommissionRates(sdk.ZeroDec(), sdk.ZeroDec(), sdk.ZeroDec())
+	msg, err := stakingtypes.NewMsgCreateValidator(valAddress, valPubKey, stakingCoin, stakingtypes.Description{}, ZeroCommission, sdk.OneInt())
+	s.Require().NoError(err)
+	res, err := stakingHandler(s.Ctx, msg)
+	s.Require().NoError(err)
+	s.Require().NotNil(res)
+
+	val, found := s.App.StakingKeeper.GetValidator(s.Ctx, valAddress)
+	s.Require().True(found)
+
+	val = val.UpdateStatus(bondStatus)
+	s.App.StakingKeeper.SetValidator(s.Ctx, val)
+
+	consAddress, err := val.GetConsAddr()
+	s.Suite.Require().NoError(err)
+
+	signingInfo := slashingtypes.NewValidatorSigningInfo(
+		consAddress,
+		s.Ctx.BlockHeight(),
+		0,
+		time.Unix(0, 0),
+		false,
+		0,
+	)
+	s.App.SlashingKeeper.SetValidatorSigningInfo(s.Ctx, consAddress, signingInfo)
+
+	return valAddress
+}
+
+// SetupMultipleValidators setups "numValidator" validators and returns their address in string
+func (s *KeeperTestSuite) SetupMultipleValidators(numValidator int) []string {
+	valAddress := []string{}
+	for i := 0; i < numValidator; i++ {
+		valAddr := s.SetupValidatorInvarient(stakingtypes.Bonded)
+		valAddress = append(valAddress, valAddr.String())
+	}
+	return valAddress
+}
