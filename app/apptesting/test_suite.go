9f1b08efa5fef63fe9dd09207da6771a5af27978
Alperen-Dere <alperen-dere@hotmail.com>
2023-08-21 11:18:59 +0300

    refactor: unused epoch data deleted from app level testing

--- app/apptesting/test_suite.go
+++ app/apptesting/test_suite.go
@@ -11,10 +11,9 @@ import (
 	"github.com/tendermint/tendermint/crypto/ed25519"
 	"github.com/tendermint/tendermint/libs/log"
 
+	"soarchain/app"
 	param "soarchain/app/params"
 
-	"soarchain/app"
-	"soarchain/x/epoch/types"
 	poatypes "soarchain/x/poa/types"
 
 	"github.com/cosmos/cosmos-sdk/crypto/keys/secp256k1"
@@ -44,39 +43,7 @@ func (s *KeeperTestSuite) Setup() {
 		Ctx:             s.Ctx,
 	}
 
-	s.SetEpoch()
 	s.TestAccs = CreateRandomAccounts(3)
-}
-
-func (s *KeeperTestSuite) SetEpoch() {
-	epochsKeeper := s.App.EpochKeeper
-	epochData := types.EpochData{
-		TotalEpochs:                   1,
-		EpochV2VRX:                    "0udmotus",
-		EpochV2VBX:                    "0udmotus",
-		EpochV2NBX:                    "0udmotus",
-		EpochRunner:                   "0udmotus",
-		EpochChallenger:               "0udmotus",
-		V2VRXTotalChallenges:          0,
-		V2VBXTotalChallenges:          0,
-		V2NBXTotalChallenges:          0,
-		ChallengerTotalChallenges:     0,
-		RunnerTotalChallenges:         0,
-		TotalChallengesPrevDay:        0,
-		InitialPerChallengeValue:      12.0,
-		V2VRXLastBlockChallenges:      100,
-		V2VRXPerChallengeValue:        2,
-		V2VBXLastBlockChallenges:      150,
-		V2VBXPerChallengeValue:        3,
-		V2NBXLastBlockChallenges:      200,
-		V2NBXPerChallengeValue:        4,
-		RunnerLastBlockChallenges:     250,
-		RunnerPerChallengeValue:       5,
-		ChallengerLastBlockChallenges: 300,
-		ChallengerPerChallengeValue:   6,
-	}
-	epochsKeeper.SetEpochData(s.Ctx, epochData)
-
 }
 
 // CreateRandomAccounts is a function return a list of randomly generated AccAddresses
