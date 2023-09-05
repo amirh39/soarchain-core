e5b48d12bc1a32b1c14a8f2e8bc13a7cf4b0110f
golnar-m <golnar@soarrobotics.com>
2023-09-06 02:23:32 +0300

    feat(didModule): add module error message file to the new did module

---
+++ x/did/errors/module_error_messages.go
@@ -0,0 +1,16 @@
+package errors
+
+var (
+	GetChallengerByType     = "[RunnerChallenge][GetChallengerByType] failed. Only registered challengers with v2n type can initiate this transaction."
+	EarnedTokenRewardsFloat = "[RunnerChallenge][V2NRewardCalculator] failed. Couldn't calcualte earned rewards."
+	NetEarnings             = "[RunnerChallenge][ParseCoinNormalized] failed. Couldn't parse and normalize a cli input for one coin type, due to invalid or an empty string."
+	EpochError              = "[RunnerChallenge][UpdateEpochRewards] failed. Couldn't updat epoch reward."
+	TotalEarnings           = "[RunnerChallenge][TotalEarnings] failed. Couldn't calculate totalEarnings with the given values."
+	NoV2nBxAddressPubKeys   = "[RunnerChallenge][v2nBxAddrCount] failed. Couldn't find client pubkeys in the tx body."
+	NotFoundAClient         = "[RunnerChallenge][GetClient] failed.  V2nBx client is not registered in the store."
+	NotFoundAValidRunner    = "[RunnerChallenge][UpdateRunner] failed.  Couldn't find a valid runner."
+
+	InvaldChallengeResult   = "[RunnerChallenge][Punish] failed. Invalid challenge result."
+	EarnedRewardsBigInt     = "[RunnerChallenge][EarnedRewardsBigInt] failed."
+
+)
