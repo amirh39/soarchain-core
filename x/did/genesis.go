183591e23626aa95047497f7f89c20b7a3159e29
golnar-m <golnar@soarrobotics.com>
2023-09-06 02:18:57 +0300

    feat(didModule): add genesis file to the new did module

---
+++ x/did/genesis.go
@@ -0,0 +1,27 @@
+package did
+
+import (
+	"soarchain/x/did/keeper"
+	"soarchain/x/did/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// InitGenesis initializes the capability module's state from a provided genesis
+// state.
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+
+	// this line is used by starport scaffolding # genesis/module/init
+	k.SetParams(ctx, genState.Params)
+}
+
+// ExportGenesis returns the capability module's exported genesis.
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
+	genesis := types.DefaultGenesis()
+	genesis.Params = k.GetParams(ctx)
+
+	genesis.DidList = k.GetAllDid(ctx)
+	// this line is used by starport scaffolding # genesis/module/export
+
+	return genesis
+}
