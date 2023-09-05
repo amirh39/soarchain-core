0657eb242f1a2c63f9d74fd940af8794781d65c4
golnar-m <golnar@soarrobotics.com>
2023-09-06 02:38:52 +0300

    feat(didModule): add deactivating  base file did transaction file to the new did module

---
+++ x/did/simulation/deactivate_did.go
@@ -0,0 +1,33 @@
+package simulation
+
+import (
+	"math/rand"
+
+	"soarchain/x/did/keeper"
+	"soarchain/x/did/types"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
+)
+
+func SimulateMsgDeactivateDid(
+	ak types.AccountKeeper,
+	bk types.BankKeeper,
+	k keeper.Keeper,
+) simtypes.Operation {
+	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simtypes.Account, chainID string,
+	) (simtypes.OperationMsg, []simtypes.FutureOperation, error) {
+		simAccount, _ := simtypes.RandomAcc(r, accs)
+		msg := &types.MsgDeactivateDid{
+			Did:                  "",
+			VerificationMethodId: "",
+			Signature:            nil,
+			FromAddress:          string(simAccount.Address),
+		}
+
+		// TODO: Handling the GenClient simulation
+
+		return simtypes.NoOpMsg(types.ModuleName, msg.Type(), "DeactivateDid simulation not implemented"), nil, nil
+	}
+}
