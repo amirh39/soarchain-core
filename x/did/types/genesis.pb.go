e3e5acf5011ae1815708b3ef6234585134a42ac2
golnar-m <golnar@soarrobotics.com>
2023-09-06 16:58:31 +0300

    refactor(didModule): replace poa -> did in comment

--- x/did/types/genesis.pb.go
+++ x/did/types/genesis.pb.go
@@ -23,7 +23,7 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
-// GenesisState defines the poa module's genesis state.
+// GenesisState defines the did module's genesis state.
 type GenesisState struct {
 	Params  Params   `protobuf:"bytes,1,opt,name=params,proto3" json:"params"`
 	DidList []string `protobuf:"bytes,2,rep,name=DidList,proto3" json:"DidList,omitempty"`
