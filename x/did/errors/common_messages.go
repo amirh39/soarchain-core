ed8c09af42dab620597d913ba7dc8acb12fd3cd6
golnar-m <golnar@soarrobotics.com>
2023-09-06 16:40:41 +0300

    refactor(didModule): refactor error message

--- x/did/errors/common_messages.go
+++ x/did/errors/common_messages.go
@@ -1,19 +1,9 @@
 package errors
 
-import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-)
-
 var (
 	ConverToString    = "[Type Error] Couldn't convert from Int to String."
 	ParseUint         = "Couldn't parse data."
 	ErrInvalidType    = "Couln't find valid type"
 	ErrInvalidAddress = "[AccAddressFromBech32] failed. Empty address string is not allowed."
-	ErrDidNotFound    = "[Did][GetDidDocument] failed. Did not found."
 	InvalidRequest    = "Invalid Request."
-	ErrDidNotActive   = "Did is Deactivated."
-)
-
-var (
-	ErrInvalidDid = sdkerrors.Register("ModuleName", 3, "Invalid Did")
 )
