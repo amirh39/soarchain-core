8805e4324d7968c7514aaffdc41308a081e1448b
golnar-m <golnar@soarrobotics.com>
2023-09-07 18:58:56 +0300

    refactor(static checks): add tests for point and isChallengeable

--- x/poa/utility/isChallengeable_test.go
+++ x/poa/utility/isChallengeable_test.go
@@ -20,16 +20,24 @@ func TestIsChallengeable(t *testing.T) {
 
 	// Test invalid score
 	isChallengeable, point, err = IsChallengeable(ctx, "invalid", "2022-01-06 11:05:17.40125 +0000 UTC", "1")
+	require.False(t, isChallengeable)
+	require.NotNil(t, point)
 	require.Error(t, err)
 
 	// Test invalid timeSinceLastChallenge
 	isChallengeable, point, err = IsChallengeable(ctx, "80", "invalid", "1")
+	require.False(t, isChallengeable)
+	require.NotNil(t, point)
 	require.Error(t, err)
 
 	// Test invalid cooldownTolerance
 	isChallengeable, point, err = IsChallengeable(ctx, "80", "2022-01-06 11:05:17.40125 +0000 UTC", "0")
+	require.False(t, isChallengeable)
+	require.NotNil(t, point)
 	require.Error(t, err)
 
 	isChallengeable, point, err = IsChallengeable(ctx, "80", "2022-01-06 11:05:17.40125 +0000 UTC", "0")
+	require.False(t, isChallengeable)
+	require.NotNil(t, point)
 	require.Error(t, err)
 }
