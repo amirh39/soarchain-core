f3614a60bf53c11a56dbad1ad4ec26bcaf7e4122
golnar-m <golnar@soarrobotics.com>
2023-09-07 18:59:42 +0300

    refactor(static checks): cleanup & remove ctx that is not used

--- x/soarmint/handler.go
+++ x/soarmint/handler.go
@@ -3,19 +3,18 @@ package soarmint
 import (
 	"fmt"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"soarchain/x/soarmint/keeper"
 	"soarchain/x/soarmint/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 // NewHandler ...
 func NewHandler(k keeper.Keeper) sdk.Handler {
 	// this line is used by starport scaffolding # handler/msgServer
 
-	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
-		ctx = ctx.WithEventManager(sdk.NewEventManager())
-
+	return func(_ sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		switch msg := msg.(type) {
 		// this line is used by starport scaffolding # 1
 		default:
