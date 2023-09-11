9242975b836a7a41e3db1bb5ba603494f6869479
golnar-m <golnar@soarrobotics.com>
2023-09-11 12:02:06 +0300

    feat(DPR module): add query file

---
+++ x/dpr/client/cli/query_drp.go
@@ -0,0 +1,73 @@
+package cli
+
+import (
+	"context"
+
+	"soarchain/x/dpr/types"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+)
+
+func CmdListDpr() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list-dpr",
+		Short: "list all dpr",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			pageReq, err := client.ReadPageRequest(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryAllDprRequest{
+				Pagination: pageReq,
+			}
+
+			res, err := queryClient.DprAll(context.Background(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddPaginationFlagsToCmd(cmd, cmd.Use)
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdShowDpr() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "show-dpr [id]",
+		Short: "shows a dpr",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			argId := args[0]
+
+			params := &types.QueryGetDprRequest{
+				Id: argId,
+			}
+
+			res, err := queryClient.Dpr(context.Background(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+	return cmd
+}
