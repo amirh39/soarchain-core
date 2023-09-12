304e49e4de5d50ac80a640c0288efad438c224c1
golnar-m <golnar@soarrobotics.com>
2023-09-12 10:36:21 +0300

    feat(dpr module): add dprd file

--- x/poa/abci.go
+++ x/poa/abci.go
@@ -21,6 +21,8 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k AppModule) {
 
 	log.Println("############## Begin Blocker Started ##############")
 
+	k.dprKeeper.DeactivateDpr(ctx, epochData.TotalEpochs)
+
 	// BeginBlocker for the PoA module. It checks if a new epoch has started and if so,
 	// it mints coins from the banking module to the PoA module according to total rewards earned during the epoch.
 
@@ -31,20 +33,12 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k AppModule) {
 
 	// check if a new epoch has started
 	if (ctx.BlockHeight()%30 == 0) && (ctx.BlockHeight() != 0) {
-		if logger != nil {
-			logger.Info("Update epoch started.", "path", "BeginBlocker")
-		}
-
-		if logger != nil {
-			logger.Info("Fetching epoch data successfully done.", "path", "BeginBlocker", "found", found, "epoch data", epochData)
-		}
 
 		k.epochKeeper.UpdateEpoch(ctx)
 
 		if logger != nil {
 			logger.Info("Update epoch  successfully done.", "path", "BeginBlocker", "epoch data", epochData, "found", found)
 		}
-
 	}
 
 	if (epochData.TotalEpochs%192 == 0) && (epochData.TotalEpochs != 0) {
@@ -55,13 +49,11 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k AppModule) {
 		}
 
 		k.epochKeeper.SetEpochData(ctx, epochData)
-
 	}
 
 	log.Println("############## End of Begin Blocker ##############")
 }
 
 func EndBlocker(ctx sdk.Context, k keeper.Keeper) []abci.ValidatorUpdate {
-
 	return []abci.ValidatorUpdate{}
 }
