e9a33c8bbb4692c4c0689f287f10bff2dbfcb8c7
golnar-m <golnar@soarrobotics.com>
2023-09-12 10:36:11 +0300

    feat(dpr module): add gen dprd file

--- x/dpr/types/message_gen_dpr.go
+++ x/dpr/types/message_gen_dpr.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"log"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
@@ -9,9 +11,15 @@ const TypeMsgGenDpr = "gen_dpr"
 
 var _ sdk.Msg = &MsgGenDpr{}
 
-func NewMsgGenDpr(id string) *MsgGenDpr {
+func NewMsgGenDpr(creator string, pidSupported_1_to_20 bool, pidSupported_21_to_40 bool, pidSupported_41_to_60 bool, vin []string, lengthOfDpr uint64) *MsgGenDpr {
+	log.Println("444444444444444444444")
 	return &MsgGenDpr{
-		Id: id,
+		Creator:              creator,
+		PidSupported_1To_20:  pidSupported_1_to_20,
+		PidSupported_21To_40: pidSupported_21_to_40,
+		PidSupported_41To_60: pidSupported_41_to_60,
+		Vin:                  vin,
+		LengthOfDpr:          lengthOfDpr,
 	}
 }
 
@@ -24,9 +32,9 @@ func (msg *MsgGenDpr) Type() string {
 }
 
 func (msg *MsgGenDpr) GetSigners() []sdk.AccAddress {
-	creator, err := sdk.AccAddressFromBech32(msg.Id)
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
-		sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "[NewMsgGenDid][AccAddressFromBech32] failed. Empty address string is not allowed.")
+		sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "[MsgGenDpr][AccAddressFromBech32] failed. Empty address string is not allowed.")
 		return nil
 	}
 	return []sdk.AccAddress{creator}
@@ -38,9 +46,9 @@ func (msg *MsgGenDpr) GetSignBytes() []byte {
 }
 
 func (msg *MsgGenDpr) ValidateBasic() error {
-	_, err := sdk.AccAddressFromBech32(msg.Id)
+	_, err := sdk.AccAddressFromBech32(msg.Creator)
 	if err != nil {
-		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "[NewMsgGenDid][ValidateBasic] failed. Invalid creator address (%s)", err)
+		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "[MsgGenDpr][ValidateBasic] failed. Invalid creator address (%s)", err)
 	}
 	return nil
 }
