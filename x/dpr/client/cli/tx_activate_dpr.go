bb45941a4eeb41bdaf72aa3515eddf8968256c74
golnar-m <golnar@soarrobotics.com>
2023-09-17 23:15:09 +0300

    feat(dpr module): add activate dpr tx cli

---
+++ x/dpr/client/cli/tx_activate_dpr.go
@@ -0,0 +1,44 @@
+package cli
+
+import (
+	"strconv"
+
+	"soarchain/x/dpr/types"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/spf13/cobra"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdActivateDpr() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "activate-dpr [pdrId]",
+		Short: "Broadcast message activate-dpr",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			argDprId := args[0]
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgActivateDpr(
+				clientCtx.GetFromAddress().String(),
+				argDprId,
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
