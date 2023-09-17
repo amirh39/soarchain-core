5adffcb666dbb8ff004aade32ddb49a05117e166
golnar-m <golnar@soarrobotics.com>
2023-09-17 23:55:40 +0300

    feat(dpr module): remove & cleanup bank module

--- testutil/keeper/did.go
+++ testutil/keeper/did.go
@@ -42,7 +42,6 @@ func DidKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 		storeKey,
 		memStoreKey,
 		paramsSubspace,
-		nil,
 	)
 
 	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
