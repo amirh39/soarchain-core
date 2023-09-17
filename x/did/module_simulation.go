9c560562a74f0db1625c93ff763ce80316134388
golnar-m <golnar@soarrobotics.com>
2023-09-17 22:37:33 +0300

    feat(dpr module): add did transactions to the file

--- x/did/module_simulation.go
+++ x/did/module_simulation.go
@@ -25,19 +25,7 @@ var (
 )
 
 const (
-	opWeightMsgGenDid = "op_weight_msg_gen_did"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgGenDid int = 100
-
-	opWeightMsgUpdateDid = "op_weight_msg_update_did"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgUpdateDid int = 100
-
-	opWeightMsgDeactivateDid = "op_weight_msg_deactivate_did"
-	// TODO: Determine the simulation weight value
-	defaultWeightMsgDeactivateDid int = 100
-
-	// this line is used by starport scaffolding # simapp/module/const
+// this line is used by starport scaffolding # simapp/module/const
 )
 
 // GenerateGenesisState creates a randomized GenState of the module
@@ -70,39 +58,6 @@ func (am AppModule) RegisterStoreDecoder(_ sdk.StoreDecoderRegistry) {}
 // WeightedOperations returns the all the gov module operations with their respective weights.
 func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
 	operations := make([]simtypes.WeightedOperation, 0)
-
-	var weightMsgGenDid int
-	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgGenDid, &weightMsgGenDid, nil,
-		func(_ *rand.Rand) {
-			weightMsgGenDid = defaultWeightMsgGenDid
-		},
-	)
-	operations = append(operations, simulation.NewWeightedOperation(
-		weightMsgGenDid,
-		didsimulation.SimulateMsgGenDid(am.accountKeeper, am.keeper),
-	))
-
-	var weightMsgUpdateDid int
-	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgUpdateDid, &weightMsgUpdateDid, nil,
-		func(_ *rand.Rand) {
-			weightMsgUpdateDid = defaultWeightMsgUpdateDid
-		},
-	)
-	operations = append(operations, simulation.NewWeightedOperation(
-		weightMsgUpdateDid,
-		didsimulation.SimulateMsgUpdateDid(am.accountKeeper, am.keeper),
-	))
-
-	var weightMsgdeactivateDid int
-	simState.AppParams.GetOrGenerate(simState.Cdc, opWeightMsgDeactivateDid, &weightMsgdeactivateDid, nil,
-		func(_ *rand.Rand) {
-			weightMsgdeactivateDid = defaultWeightMsgDeactivateDid
-		},
-	)
-	operations = append(operations, simulation.NewWeightedOperation(
-		weightMsgdeactivateDid,
-		didsimulation.SimulateMsgDeactivateDid(am.accountKeeper, am.keeper),
-	))
 
 	// this line is used by starport scaffolding # simapp/module/operation
 
