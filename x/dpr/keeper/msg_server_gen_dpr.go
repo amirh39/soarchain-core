3a8b336046bb4319a05490d555bbe1e80da46dd9
golnar-m <golnar@soarrobotics.com>
2023-09-17 22:58:59 +0300

    feat(dpr module): add gen dpr tx

--- x/dpr/keeper/msg_server_gen_dpr.go
+++ x/dpr/keeper/msg_server_gen_dpr.go
@@ -7,64 +7,52 @@ import (
 	"soarchain/x/dpr/types"
 	"soarchain/x/dpr/utility"
 
-	"github.com/pborman/uuid"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/google/uuid"
 )
 
 func (k msgServer) GenDpr(goCtx context.Context, msg *types.MsgGenDpr) (*types.MsgGenDprResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	logger := k.Logger(ctx)
 
-	log.Println("############## Generating a dpr Transaction is Started ##############")
-
 	epochDtata, found := k.epochKeeper.GetEpochData(ctx)
 	if !found {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[GenDpr][GetEpochData] failed. Couldn't find epoch data.")
 	}
 
-	if logger != nil {
-		logger.Info("epochDtata.TotalEpochs--===>", "transaction", "GenDpr", "epochDtata.TotalEpochs", epochDtata.TotalEpochs)
-	}
-
-	result := k.VerifyDPRInputs(msg, epochDtata.TotalEpochs)
+	result := k.VerifyDprInputs(msg, epochDtata.TotalEpochs)
 	if !result {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[GenDpr][VerifyDPRInputs] failed. Make sure you are using valid properties for creating Dpr object.")
+		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[GenDpr][VerifyDprInputs] failed. Make sure you are using valid inputs for creating Dpr object.")
 	}
 
 	if logger != nil {
-		logger.Info("Verifying a received DPR sucessfully done.", "transaction", "GenDpr")
+		logger.Info("Validationg DPR is successfully Done.", "transaction", "GenDpr")
 	}
 
-	found = k.didKeeper.FindEligibleDid(ctx, utility.CalculatePinNumber(msg))
-	if !found {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[GenDpr][FindEligibleDid] failed. There is no eligible client to serve this DPR.")
+	pinNumbers := utility.CalculatePinNumber(msg)
+
+	eligible := k.Keeper.didKeeper.GetEligibleDids(ctx, pinNumbers)
+	if !eligible {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[GenDpr][GetEligibleDids] failed. There is no eligible client to serve this DPR.")
 	}
 
-	if logger != nil {
-		logger.Info("Finding eligible client for serving DPR sucessfully done.", "transaction", "GenDpr", "found", found)
-	}
-
-	// Save dpr into storage
+	//Save dpr into storage
 	newDpr := types.Dpr{
-		Id:                            uuid.NewRandom().String(),
+		Id:                            uuid.New().String(),
 		Creator:                       msg.Creator,
 		PidSupportedOneToTwnety:       msg.PidSupportedOneToTwnety,
 		PidSupportedTwentyOneToForthy: msg.PidSupportedTwentyOneToForthy,
 		PidSupportedForthyOneToSixty:  msg.PidSupportedForthyOneToSixty,
-		IsActive:                      true,
+		IsActive:                      false,
 		Vin:                           []string{},
 		ClientPubkeys:                 []string{},
 		LengthOfDpr:                   msg.LengthOfDpr,
 	}
-
 	k.SetDpr(ctx, newDpr)
 
-	xx, _ := k.GetDpr(ctx, newDpr.Id)
-
 	if logger != nil {
-		logger.Info("saved dpr id fetched.", "transaction", "GenDpr", "xx", xx)
+		logger.Info("Generating DPR is successfully Done.", "transaction", "GenDpr")
 	}
 
 	log.Println("############## End of Generating dpr Transaction ##############")
