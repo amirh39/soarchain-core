c994e142ce0f381f86e11541c140172787c87cc3
golnar-m <golnar@soarrobotics.com>
2023-09-20 12:30:10 +0300

    refactor(dpr module): add more description to the logs

--- x/poa/abci.go
+++ x/poa/abci.go
@@ -19,14 +19,14 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k AppModule) {
 
 	logger := k.keeper.Logger(ctx)
 
-	log.Println("############## Begin Blocker Started ##############")
+	log.Println("############## Poa Module Begin Blocker is Started ##############")
 
 	// BeginBlocker for the PoA module. It checks if a new epoch has started and if so,
 	// it mints coins from the banking module to the PoA module according to total rewards earned during the epoch.
 
 	err := k.keeper.MintRewardCoins(ctx, epochData)
 	if err != nil {
-		logger.Info("Mint Reward Coins failed.", "path", "BeginBlocker")
+		logger.Info("[Poa Module][BeginBlocker] Mint Reward Coins failed.", "path", "BeginBlocker")
 	}
 
 	// check if a new epoch has started
@@ -36,13 +36,13 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k AppModule) {
 		}
 
 		if logger != nil {
-			logger.Info("Fetching epoch data successfully done.", "path", "BeginBlocker", "found", found, "epoch data", epochData)
+			logger.Info("[Poa Module][BeginBlocker] Fetching epoch data successfully done.", "path", "BeginBlocker", "found", found, "epoch data", epochData)
 		}
 
 		k.epochKeeper.UpdateEpoch(ctx)
 
 		if logger != nil {
-			logger.Info("Update epoch  successfully done.", "path", "BeginBlocker", "epoch data", epochData, "found", found)
+			logger.Info("[Poa Module][BeginBlocker] Update epoch successfully done.", "path", "BeginBlocker", "epoch data", epochData, "found", found)
 		}
 
 	}
@@ -51,14 +51,14 @@ func BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock, k AppModule) {
 
 		epochData, err = k.keeper.ComputeAdaptiveHalving(ctx, epochData)
 		if logger != nil && err == nil {
-			logger.Info("Compute Adaptive Halving successfully done.", "path", "BeginBlocker")
+			logger.Info("[Poa Module][BeginBlocker] Compute Adaptive Halving successfully done.", "path", "BeginBlocker")
 		}
 
 		k.epochKeeper.SetEpochData(ctx, epochData)
 
 	}
 
-	log.Println("############## End of Begin Blocker ##############")
+	log.Println("############## End of Poa Module Begin Blocker ##############")
 }
 
 func EndBlocker(ctx sdk.Context, k keeper.Keeper) []abci.ValidatorUpdate {
