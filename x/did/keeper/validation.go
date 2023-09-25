48d09c3e20469ef352699ec18d304cec32844f14
golnar-m <golnar@soarrobotics.com>
2023-09-26 02:56:11 +0300

    feat(merge-did-client): add validation

---
+++ x/did/keeper/validation.go
@@ -0,0 +1,105 @@
+package keeper
+
+import (
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+	"soarchain/x/did/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+func (k Keeper) ValidateInputs(msg *types.MsgGenDid) bool {
+
+	if msg.Creator == "" || msg.Certificate == "" || msg.ClientSignature == "" || msg.Document.VerificationMethods[0].Id == "" {
+		return false
+	}
+
+	if msg.Signature == nil || msg.Document == nil {
+		return false
+	}
+
+	return true
+}
+
+func (k Keeper) IsUniqueDid(ctx sdk.Context, id string, address string, pubkey string) bool {
+	result := k.ValidateDid(ctx, id, address, pubkey)
+	return result
+}
+
+func CreateX509CertFromString(certString string) (*x509.Certificate, error) {
+	deviceCertPEM := []byte(certString)
+	deviceBlock, rest := pem.Decode(deviceCertPEM)
+	if deviceBlock == nil && rest != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidType, "[CreateX509CertFromString][pem.Decode] failed. Invalid device certificate.")
+	}
+	deviceCert, err := x509.ParseCertificate(deviceBlock.Bytes)
+	if err != nil {
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "[CreateX509CertFromString][ParseCertificate] failed. Invalid parsing certificate. Error: [ %T ]", err)
+	}
+	return deviceCert, nil
+}
+
+func ValidateX509CertByASN1(creator string, signature string, deviceCert *x509.Certificate) bool {
+
+	if deviceCert == nil || creator == "" {
+		return false
+	}
+
+	signatureBytes, err := hex.DecodeString(signature)
+	if err != nil {
+		return false
+	}
+
+	hashedAddr := sha256.Sum256([]byte(creator))
+
+	if deviceCert.PublicKeyAlgorithm == x509.ECDSA {
+
+		if ecdsaPubKey, ok := deviceCert.PublicKey.(*ecdsa.PublicKey); ok {
+
+			if ecdsa.VerifyASN1(ecdsaPubKey, hashedAddr[:], signatureBytes) {
+				// signature is valid
+			} else {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+	return true
+}
+
+func ExtractPubkeyFromCertificate(certificate string) (string, error) {
+
+	if certificate == "" {
+		return "", sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "[GenDid][ExtractPubkeyFromCertificate] failed. Device certification is not valid.")
+	}
+
+	deviceCertificate, error := CreateX509CertFromString(certificate)
+	if error != nil {
+		return "", sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "[GenDid][CreateX509CertFromString] failed. Invalid device certificate.")
+	}
+
+	pubKeyFromCertificate, err := x509.MarshalPKIXPublicKey(deviceCertificate.PublicKey)
+	if err != nil {
+		return "", sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "[ExtractPubkeyFromCertificate][MarshalPKIXPublicKey] failed. Couldn't extract a public key from device certificate. Error: [ %T ]", err)
+	}
+
+	pubKeyHex := hex.EncodeToString(pubKeyFromCertificate)
+	if pubKeyHex == "" {
+		return "", sdkerrors.Wrapf(sdkerrors.ErrInvalidType, "[ExtractPubkeyFromCertificate][EncodeToString] failed. Couldn't encode public Key to hex string. Error: [ %T ]", err)
+	}
+	return pubKeyHex, nil
+}
+
+func ValidateX509Cert(derivedCert *x509.Certificate, signerCert *x509.Certificate) (bool, error) {
+	err := derivedCert.CheckSignatureFrom(signerCert)
+	if err != nil {
+		return false, err
+	} else {
+		return true, nil
+	}
+}
