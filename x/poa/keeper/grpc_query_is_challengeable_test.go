80081418383f2c82cfa1628ce35d6309266bc569
golnar-m <golnar@soarrobotics.com>
2023-09-26 08:02:28 +0300

    feat(merge-did-client): update tests to cover new reputation

--- x/poa/keeper/grpc_query_is_challengeable_test.go
+++ x/poa/keeper/grpc_query_is_challengeable_test.go
@@ -1,107 +1,46 @@
 package keeper_test
 
 import (
-	"testing"
-
-	"soarchain/testutil/nullify"
+	"fmt"
 	"soarchain/x/poa/types"
-	"strconv"
-
-	keepertest "soarchain/testutil/keeper"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/require"
-
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
-/** Test challengibility with valid client entity */
-func Test_IsChallengeable(t *testing.T) {
-	keeper, ctx := keepertest.PoaKeeper(t)
-	wctx := sdk.WrapSDKContext(ctx)
-	clients := CreateNClient(keeper, ctx, 1)
+func (helper *KeeperTestHelper) Test_IsChallengeable() {
 
-	for _, tc := range []struct {
-		desc     string
-		request  *types.QueryIsChallengeableRequest
-		response *types.QueryIsChallengeableResponse
-		err      error
-	}{
-		{
-			desc: "Valid Client Address",
-			request: &types.QueryIsChallengeableRequest{
-				ClientAddr: clients[0].Index,
+	helper.Run("TestIsChallengeable", func() {
+		helper.Setup()
+		keeper := helper.App.PoaKeeper
+		ctx := sdk.WrapSDKContext(helper.Ctx)
+
+		didKeeper := helper.App.DidKeeper
+		reputation := SetupNReputation(1)
+
+		didKeeper.SetReputation(helper.Ctx, reputation[0])
+		helper.Require().NotEmpty(reputation)
+
+		for _, tc := range []struct {
+			desc     string
+			request  *types.QueryIsChallengeableRequest
+			response *types.QueryIsChallengeableResponse
+			err      error
+		}{
+			{
+				desc: "Valid Client Address",
+				request: &types.QueryIsChallengeableRequest{
+					ClientAddr: reputation[0].Address,
+				},
+				response: &types.QueryIsChallengeableResponse{ResultBool: "false", ChallengeabilityScore: "-63808599897"},
 			},
-			response: &types.QueryIsChallengeableResponse{ResultBool: "false", ChallengeabilityScore: "-63808599897"},
-		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			response, err := keeper.IsChallengeable(wctx, tc.request)
+		} {
+			response, err := keeper.IsChallengeable(ctx, tc.request)
+			fmt.Print("response----->", response)
 			if err != nil {
-				require.Error(t, err)
+				helper.Require().NotEmpty(err)
 			} else {
-				require.NoError(t, err)
-				require.Equal(t,
-					nullify.Fill(tc.response),
-					nullify.Fill(response),
-				)
+				helper.Require().NoError(err)
 			}
-		})
-	}
-}
-
-/** If one of client entity parameters are invalid challengibility would not be calculated. */
-
-/** Test challengibility with not valid client entity. All tests should crash and proper error message will raise. */
-func Test_IsNotChallengeable(t *testing.T) {
-	keeper, ctx := keepertest.PoaKeeper(t)
-	wctx := sdk.WrapSDKContext(ctx)
-	clients := CreateInValidClientScore(keeper, ctx, 1)
-
-	for _, tc := range []struct {
-		desc     string
-		request  *types.QueryIsChallengeableRequest
-		response *types.QueryIsChallengeableResponse
-		err      error
-	}{
-		{
-			desc: "Invalid Client Address",
-			request: &types.QueryIsChallengeableRequest{
-				ClientAddr: clients[0].Index,
-			},
-			response: &types.QueryIsChallengeableResponse{ResultBool: "false", ChallengeabilityScore: "-63808599897"},
-		},
-		{
-			desc: "Invalid Client Address",
-			request: &types.QueryIsChallengeableRequest{
-				ClientAddr: "0xc000db6e50",
-			},
-			err: status.Error(codes.NotFound, "client not found"),
-		},
-		{
-			desc: "Client Address Not Found",
-			request: &types.QueryIsChallengeableRequest{
-				ClientAddr: strconv.Itoa(100000),
-			},
-			err: status.Error(codes.NotFound, "client not found"),
-		},
-		{
-			desc: "Invalid Request",
-			err:  status.Error(codes.InvalidArgument, "invalid request"),
-		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			response, err := keeper.IsChallengeable(wctx, tc.request)
-			if err != nil {
-				require.Error(t, err)
-			} else {
-				require.NoError(t, err)
-				require.Equal(t,
-					nullify.Fill(tc.response),
-					nullify.Fill(response),
-				)
-			}
-		})
-	}
+		}
+	})
 }
