f011ed2c10e5c9145f63625b16f573e691c4579e
golnar-m <golnar@soarrobotics.com>
2023-09-26 07:59:26 +0300

    feat(merge-did-client): update reputation and delete client to cover new reputation

--- x/poa/keeper/grpc_query_is_challengeable.go
+++ x/poa/keeper/grpc_query_is_challengeable.go
@@ -18,26 +18,25 @@ func (k Keeper) IsChallengeable(goCtx context.Context, req *types.QueryIsChallen
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "[IsChallengeable] failed. Invalid request: %T.", req)
 	}
 
-	if !utility.ValidString(req.ClientAddr ) {
+	if !utility.ValidString(req.ClientAddr) {
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidPubKey, "[IsChallengeable][ValidString] failed. Couldn't find a valid client address from the request. got: [ %T ], Make sure client address is not empty OR invalid.", req.ClientAddr)
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	client, isFound := k.GetClient(ctx, req.ClientAddr)
+	reputation, isFound := k.didKeeper.GetReputationByClientAddress(ctx, req.ClientAddr)
 	if !isFound {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, "[IsChallengeable][GetClient] failed. Target client is not registered in the store.")
 	}
 
 	// Check challengeability
-	isChallengeable, point, err := utility.IsChallengeable(ctx, client.Score, client.LastTimeChallenged, client.CoolDownTolerance)
+	isChallengeable, point, err := utility.IsChallengeable(ctx, reputation.Score, reputation.LastTimeChallenged, reputation.CoolDownTolerance)
 	if err != nil {
-		return nil, sdkerrors.Wrapf(sdkerrors.ErrLogic, "[IsChallengeable][utility.IsChallengeable] failed. Couldn't calculate challengeability from the client entity. got [ %T ], Make sure the client entity includes valid Score, LastTimeChallenged and CoolDownTolerance. ", client)
+		return nil, sdkerrors.Wrapf(sdkerrors.ErrLogic, "[IsChallengeable][utility.IsChallengeable] failed. Couldn't calculate challengeability from the client entity. got [ %T ], Make sure the client entity includes valid Score, LastTimeChallenged and CoolDownTolerance. ", reputation)
 	}
 
 	isChallengeableStr := strconv.FormatBool(isChallengeable)
 	pointString := strconv.FormatFloat(point, 'f', -1, 64)
 
 	return &types.QueryIsChallengeableResponse{ResultBool: isChallengeableStr, ChallengeabilityScore: pointString}, nil
-
 }
