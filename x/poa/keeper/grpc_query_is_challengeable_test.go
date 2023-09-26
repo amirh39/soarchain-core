358b98fbfa3190eb2c6ae129d39e0c394e989b27
golnar-m <golnar@soarrobotics.com>
2023-09-26 18:57:53 +0300

    feat(merging_client_did): cleanup client with reputation

--- x/poa/keeper/grpc_query_is_challengeable_test.go
+++ x/poa/keeper/grpc_query_is_challengeable_test.go
@@ -1,46 +1,102 @@
 package keeper_test
 
 import (
-	"fmt"
+	keepertest "soarchain/testutil/keeper"
+	"soarchain/testutil/nullify"
 	"soarchain/x/poa/types"
+	"strconv"
+	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
-func (helper *KeeperTestHelper) Test_IsChallengeable() {
+/** Test challengibility with valid reputation entity */
+func Test_IsChallengeable(t *testing.T) {
+	keeper, ctx := keepertest.PoaKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+	reputations := CreateNReputation(keeper, ctx, 1)
 
-	helper.Run("TestIsChallengeable", func() {
-		helper.Setup()
-		keeper := helper.App.PoaKeeper
-		ctx := sdk.WrapSDKContext(helper.Ctx)
-
-		didKeeper := helper.App.DidKeeper
-		reputation := SetupNReputation(1)
-
-		didKeeper.SetReputation(helper.Ctx, reputation[0])
-		helper.Require().NotEmpty(reputation)
-
-		for _, tc := range []struct {
-			desc     string
-			request  *types.QueryIsChallengeableRequest
-			response *types.QueryIsChallengeableResponse
-			err      error
-		}{
-			{
-				desc: "Valid Client Address",
-				request: &types.QueryIsChallengeableRequest{
-					ClientAddr: reputation[0].Address,
-				},
-				response: &types.QueryIsChallengeableResponse{ResultBool: "false", ChallengeabilityScore: "-63808599897"},
+	for _, tc := range []struct {
+		desc     string
+		request  *types.QueryIsChallengeableRequest
+		response *types.QueryIsChallengeableResponse
+		err      error
+	}{
+		{
+			desc: "Valid reputation Address",
+			request: &types.QueryIsChallengeableRequest{
+				ClientAddr: reputations[0].Address,
 			},
-		} {
-			response, err := keeper.IsChallengeable(ctx, tc.request)
-			fmt.Print("response----->", response)
+			response: &types.QueryIsChallengeableResponse{ResultBool: "false", ChallengeabilityScore: "-63808599897"},
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			response, err := keeper.IsChallengeable(wctx, tc.request)
 			if err != nil {
-				helper.Require().NotEmpty(err)
+				require.Error(t, err)
 			} else {
-				helper.Require().NoError(err)
+				require.NoError(t, err)
+				require.Equal(t,
+					nullify.Fill(tc.response),
+					nullify.Fill(response),
+				)
 			}
-		}
-	})
+		})
+	}
+}
+
+/** If one of reputation entity parameters are invalid challengibility would not be calculated. */
+func Test_IsNotChallengeable(t *testing.T) {
+	keeper, ctx := keepertest.PoaKeeper(t)
+	wctx := sdk.WrapSDKContext(ctx)
+	reputations := CreateInValidReputationScore(keeper, ctx, 1)
+
+	for _, tc := range []struct {
+		desc     string
+		request  *types.QueryIsChallengeableRequest
+		response *types.QueryIsChallengeableResponse
+		err      error
+	}{
+		{
+			desc: "Invalid reputation Address",
+			request: &types.QueryIsChallengeableRequest{
+				ClientAddr: reputations[0].Address,
+			},
+			response: &types.QueryIsChallengeableResponse{ResultBool: "false", ChallengeabilityScore: "-63808599897"},
+		},
+		{
+			desc: "Invalid reputation Address",
+			request: &types.QueryIsChallengeableRequest{
+				ClientAddr: "0xc000db6e50",
+			},
+			err: status.Error(codes.NotFound, "reputation not found"),
+		},
+		{
+			desc: "reputation Address Not Found",
+			request: &types.QueryIsChallengeableRequest{
+				ClientAddr: strconv.Itoa(100000),
+			},
+			err: status.Error(codes.NotFound, "reputation not found"),
+		},
+		{
+			desc: "Invalid Request",
+			err:  status.Error(codes.InvalidArgument, "invalid request"),
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			response, err := keeper.IsChallengeable(wctx, tc.request)
+			if err != nil {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t,
+					nullify.Fill(tc.response),
+					nullify.Fill(response),
+				)
+			}
+		})
+	}
 }
