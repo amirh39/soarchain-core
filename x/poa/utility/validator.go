b0f7051271c80286f4bb7123eb7f9ce48b364561
golnar-m <golnar@soarrobotics.com>
2023-10-09 02:02:35 +0300

    feat(merging-runner-challenger-did): cleanup unused functionalities

--- x/poa/utility/validator.go
+++ x/poa/utility/validator.go
@@ -4,22 +4,6 @@ import "soarchain/x/poa/utility/utilConstants"
 
 var addressLength = utilConstants.AddressLength
 
-/** ValidPublickey performs the validation of a publickey */
-func ValidPubkey(input string) bool { // TODO: Add validation of an object Pubkey
-	if len(input) == 0 || input == "" {
-		return false
-	}
-	return true
-}
-
-/** ValidAddress performs the validation of an address */
-func ValidAddress(input string) bool {
-	if len(input) > addressLength || len(input) < addressLength || input == "" || input[0:4] != "soar" {
-		return false
-	}
-	return true
-}
-
 /** ValidString performs the validation of a string input */
 func ValidString(input string) bool {
 	if len(input) == 0 || input == "" {
--- x/poa/utility/validator_test.go
+++ x/poa/utility/validator_test.go
@@ -1,47 +1,10 @@
 package utility
 
 import (
-	"soarchain/x/poa/utility/utilConstants"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
-
-func Test_ValidPubkey(t *testing.T) {
-	r := require.New(t)
-
-	pubkey := utilConstants.PubKey
-	result := ValidPubkey(pubkey)
-	r.Equal(result, true)
-
-}
-
-func Test_NotValidPubkey(t *testing.T) {
-	r := require.New(t)
-
-	emptyPublicKey := ""
-	r.Equal(ValidPubkey(emptyPublicKey), false)
-}
-
-func Test_ValidAddress(t *testing.T) {
-	r := require.New(t)
-
-	address := utilConstants.Address
-	result := ValidAddress(address)
-	r.Equal(result, true)
-	r.Equal(len(address), 43)
-	r.Equal(address[0:4], "soar")
-}
-
-func Test_NotValidAddress(t *testing.T) {
-	r := require.New(t)
-
-	emptyAddress := ""
-	r.Equal(ValidAddress(emptyAddress), false)
-
-	shortAddress := ""
-	r.Equal(ValidAddress(shortAddress), false)
-}
 
 func Test_ValidString(t *testing.T) {
 	r := require.New(t)
