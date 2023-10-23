8e57161f1fbe13cb5652e6a5bd1236803e6bef00
golnar-m <131612838+golnar-m@users.noreply.github.com>
2023-10-23 21:52:38 +0300

    Update error message

--- x/did/keeper/challenger_did.go
+++ x/did/keeper/challenger_did.go
@@ -29,6 +29,23 @@ func (k Keeper) GetChallengerDid(ctx sdk.Context, Address string) (val types.Cha
 	return val, true
 }
 
+func (k Keeper) GetChallengerDidId(ctx sdk.Context, id string) (clientDid types.ChallengerDid, found bool) {
+	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.DidKeyPrefix))
+	iterator := sdk.KVStorePrefixIterator(store, []byte{})
+
+	defer iterator.Close()
+
+	for ; iterator.Valid(); iterator.Next() {
+		var val types.ChallengerDid
+		k.cdc.MustUnmarshal(iterator.Value(), &val)
+		if val.Id == id {
+			return val, true
+		}
+	}
+
+	return types.ChallengerDid{}, false
+}
+
 func (k Keeper) GetAllChallengerDid(ctx sdk.Context) (list []types.ChallengerDid) {
 	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.ChallengerDidKeyPrefix))
 	iterator := sdk.KVStorePrefixIterator(store, []byte{})
