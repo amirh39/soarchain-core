90dccde37d35a86f0354a04a84be5f695a219eff
Alperen-Dere <alperen-dere@hotmail.com>
2023-10-24 10:34:15 +0300

    fix: non-deterministic uuid fixed

--- x/dpr/keeper/msg_server_gen_dpr.go
+++ x/dpr/keeper/msg_server_gen_dpr.go
@@ -2,25 +2,26 @@ package keeper
 
 import (
 	"context"
+	"crypto/sha256"
+	"encoding/hex"
 	"log"
 
 	"soarchain/x/dpr/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	"github.com/google/uuid"
 )
 
 func (k msgServer) GenDpr(goCtx context.Context, msg *types.MsgGenDpr) (*types.MsgGenDprResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 	logger := k.Logger(ctx)
 
-	epochDtata, found := k.epochKeeper.GetEpochData(ctx)
+	epochData, found := k.epochKeeper.GetEpochData(ctx)
 	if !found {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[GenDpr][GetEpochData] failed. Couldn't find epoch data.")
 	}
 
-	result := k.VerifyDprInputs(msg, epochDtata.TotalEpochs)
+	result := k.VerifyDprInputs(msg, epochData.TotalEpochs)
 	if !result {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[GenDpr][VerifyDprInputs] failed. Make sure you are using valid inputs for creating Dpr object.")
 	}
@@ -29,9 +30,14 @@ func (k msgServer) GenDpr(goCtx context.Context, msg *types.MsgGenDpr) (*types.M
 		logger.Info("Validating DPR is successfully Done.", "transaction", "GenDpr")
 	}
 
+	timestampStr := ctx.BlockTime().String()
+	hashPayload := msg.Creator + "|" + timestampStr
+	hash := sha256.Sum256([]byte(hashPayload))
+	dprID := hex.EncodeToString(hash[:])
+
 	//Save dpr into storage
 	newDpr := types.Dpr{
-		Id:            uuid.New().String(),
+		Id:            dprID,
 		Creator:       msg.Creator,
 		SupportedPIDs: msg.SupportedPIDs,
 		IsActive:      false,
