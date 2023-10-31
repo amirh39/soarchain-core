d02b7c6de2d0704fb67e26d648f1d2f75d2592de
Alperen-Dere <alperen-dere@hotmail.com>
2023-10-31 13:56:25 +0300

    feat: unix-time calculation implemented

--- x/dpr/utility/dprEndTime_test.go
+++ x/dpr/utility/dprEndTime_test.go
@@ -4,26 +4,66 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 )
 
+// TestCalculateDPREndTime checks the determinism and correctness of CalculateDPREndTime function
 func TestCalculateDPREndTime(t *testing.T) {
-	// Define the block time (e.g., current time)
-	blockTime := time.Now()
+	// Define test cases
+	tests := []struct {
+		name      string
+		blockTime time.Time
+		epochs    int
+		want      int64
+		wantErr   bool
+		repeat    int // Number of times to repeat the function call for determinism check
+	}{
+		{
+			name:      "Positive Epochs",
+			blockTime: time.Date(2023, 10, 31, 12, 0, 0, 0, time.UTC),
+			epochs:    10,
+			want:      1698758100, // Expected Unix time
+			wantErr:   false,
+			repeat:    5,
+		},
+		{
+			name:      "Zero Epochs",
+			blockTime: time.Date(2023, 10, 31, 12, 0, 0, 0, time.UTC),
+			epochs:    0,
+			want:      1698753600, // Expected Unix time
+			wantErr:   false,
+			repeat:    5,
+		},
+		{
+			name:      "Negative Epochs",
+			blockTime: time.Date(2023, 10, 31, 12, 0, 0, 0, time.UTC),
+			epochs:    -1,
+			want:      0,
+			wantErr:   true,
+			repeat:    5,
+		},
+		{
+			name:      "Large Number of Epochs",
+			blockTime: time.Date(2023, 1, 1, 0, 0, 0, 0, time.UTC),
+			epochs:    100000,
+			want:      1717531200, // Expected Unix time
+			wantErr:   false,
+			repeat:    5,
+		},
+	}
 
-	// Define the number of epochs for testing
-	epochs := 10
-
-	// Call the function to calculate the DPREndTime
-	dprEndTimeStr, err := CalculateDPREndTime(blockTime, epochs)
-
-	// Check for errors
-	require.NoError(t, err)
-
-	// Calculate the expected DPREndTime based on the provided number of epochs
-	expectedDPREndTime := blockTime.Add(time.Duration(epochs*BlocksPerEpoch*SecondsPerBlock) * time.Second)
-	expectedDPREndTimeStr := expectedDPREndTime.Format("2006-01-02 15:04 +0000 UTC")
-
-	// Compare the calculated result with the expected result
-	require.Equal(t, expectedDPREndTimeStr, dprEndTimeStr)
+	// Run test cases
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for i := 0; i < tt.repeat; i++ {
+				got, err := CalculateDPREndTime(tt.blockTime, tt.epochs)
+				if tt.wantErr {
+					assert.Error(t, err)
+				} else {
+					assert.NoError(t, err)
+					assert.Equal(t, tt.want, got, "Run %d failed for test %s", i+1, tt.name)
+				}
+			}
+		})
+	}
 }
