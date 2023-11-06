8e4d2ebcb42259f3d4bb2d007d529ac8338c8579
Alperen-Dere <alperen-dere@hotmail.com>
2023-11-06 09:05:42 +0300

    feat: payment algorithm

--- x/dpr/keeper/payment.go
+++ x/dpr/keeper/payment.go
@@ -1,72 +1,96 @@
 package keeper
 
 import (
-	"fmt"
-	"math/big"
+	"log"
 	"soarchain/app/params"
-	"soarchain/x/poa/errors"
+	"soarchain/x/did/types"
+	"strconv"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-func (k Keeper) DistributeRewards(ctx sdk.Context) {
-	logger := k.Logger(ctx)
-	activeDprs := k.GetAllActiveDpr(ctx)
-
-	if activeDprs == nil {
-		logger.Info("There is no active DPR", "path", "DistributeRewards")
-		return
+func (k Keeper) DistributeRewards(ctx sdk.Context, clientDid types.ClientDid) (sdk.DecCoins, error) {
+	// Check if there are any DprInfos in the client DID
+	if len(clientDid.GetDprInfos()) == 0 {
+		// If there are no DprInfos, exit the function.
+		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[DistributeRewards] No DPRs found in Client DID")
 	}
 
-	for _, dpr := range activeDprs {
-		budget, _ := sdk.ParseCoinNormalized(dpr.DprBudget)
+	var totalRewards sdk.DecCoins
 
-		// Convert budget.Amount to big.Int for precise calculations
-		budgetAmountBigInt := big.NewInt(budget.Amount.Int64())
+	// Get current epoch from the context or state
+	currentEpoch, _ := k.epochKeeper.GetEpochData(ctx)
 
-		// Calculate reward per epoch using big.Int
-		rewardPerEpochBigInt := new(big.Int).Div(budgetAmountBigInt, big.NewInt(int64(dpr.Duration)))
+	// Iterate over the DPRs the client is in
+	for i, dprInfo := range clientDid.DprInfos {
+		// Retrieve DPR using dprInfo.Id
+		dpr, _ := k.GetDpr(ctx, dprInfo.Id)
 
-		// Calculate the reward per client using big.Int
-		rewardPerClientBigInt := new(big.Int).Div(rewardPerEpochBigInt, big.NewInt(int64(dpr.MaxClientCount)))
-
-		for _, clientPubKey := range dpr.ClientPubkeys {
-			// Convert the rewardPerClientBigInt back to sdk.Int for creating sdk.Coin
-			rewardPerClientAmount := sdk.NewIntFromBigInt(rewardPerClientBigInt)
-
-			// Distribute 'rewardPerClient' to the client identified by 'clientPubKey'
-			rewardPerClient := sdk.NewCoin(budget.Denom, rewardPerClientAmount)
-			client, _ := k.poaKeeper.GetReputation(ctx, clientPubKey)
-			netEarnings, _ := sdk.ParseCoinNormalized(client.DprEarnings)
-
-			totalEarnings := netEarnings.Add(rewardPerClient)
-
-			client.DprEarnings = totalEarnings.String()
-
-			k.poaKeeper.SetReputation(ctx, client)
-
-			// Example print statement (replace with actual distribution logic)
-			fmt.Printf("Distributing %s to client with pubkey: %s\n", rewardPerClient, clientPubKey)
+		// Calculate total reward possible until the current epoch
+		elapsedEpochs := currentEpoch.TotalEpochs - dpr.DprStartEpoch
+		if elapsedEpochs > dpr.Duration {
+			elapsedEpochs = dpr.Duration // Cap it at the duration
 		}
+		log.Println(dpr.DprBudget)
+
+		dprBudgetCoin, err := sdk.ParseCoinNormalized(dpr.DprBudget)
+		if err != nil {
+			log.Printf("Error parsing DPR budget: %v\n", err)
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "[DistributeRewards] Error parsing DPR budget.")
+		}
+
+		dprBudget, err := strconv.ParseInt(dprBudgetCoin.Amount.String(), 10, 64)
+		if err != nil {
+			log.Printf("Error converting DPR budget amount to int: %v\n", err)
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "[DistributeRewards] Error converting DPR budget amount.")
+		}
+
+		dprBudgetDec := sdk.NewDec(dprBudget)
+		log.Println(dprBudgetDec)
+		rewardPerEpoch := sdk.NewDecCoinFromDec(params.BondDenom, dprBudgetDec.Quo(sdk.NewDec(int64(dpr.MaxClientCount))).Quo(sdk.NewDec(int64(dpr.Duration))))
+		log.Println(rewardPerEpoch)
+		if rewardPerEpoch.IsNegative() || rewardPerEpoch.IsZero() {
+			log.Printf("Reward per epoch is not positive: %s\n", rewardPerEpoch)
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "[DistributeRewards] Reward per epoch is not positive.")
+		}
+
+		totalPossibleRewardDec := rewardPerEpoch.Amount.MulInt64(int64(elapsedEpochs))
+		log.Println(totalPossibleRewardDec)
+		if totalPossibleRewardDec.IsNegative() {
+			log.Printf("Total possible reward is negative: %s\n", totalPossibleRewardDec)
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "[DistributeRewards] Total possible reward is negative.")
+		}
+		totalPossibleReward := sdk.NewDecCoinFromDec(params.BondDenom, totalPossibleRewardDec)
+		log.Println(totalPossibleReward)
+		if !totalPossibleReward.IsValid() {
+			log.Printf("Total possible reward is invalid: %s\n", totalPossibleReward)
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, "[DistributeRewards] Total possible reward is invalid.")
+		}
+		// Subtract the amount already claimed
+		claimedAmount, ok := sdk.NewIntFromString(dprInfo.Claimed)
+		log.Println(claimedAmount)
+		if !ok {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "[DistributeRewards] Invalid claimed amount")
+		}
+		rewardToDistribute := totalPossibleReward.Sub((sdk.NewDecCoin(params.BondDenom, claimedAmount)))
+		log.Println(rewardToDistribute)
+		currentClaimedAmount, ok := sdk.NewIntFromString(dprInfo.Claimed)
+		if !ok {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "[DistributeRewards] Invalid claimed amount")
+		}
+
+		// Calculate the new claimed amount by adding the rewardToDistribute to the current claimed amount
+		newClaimedAmount := currentClaimedAmount.Add(rewardToDistribute.Amount.RoundInt())
+		log.Println(newClaimedAmount)
+		// Update the claimed amount in the specific DprInfo object
+		clientDid.DprInfos[i].Claimed = newClaimedAmount.String()
+		k.didKeeper.SetClientDid(ctx, clientDid)
+		// Add the calculated reward to the total rewards
+		totalRewards = totalRewards.Add(rewardToDistribute)
+
 	}
 
-	if logger != nil {
-		logger.Info("Payments of the DPR successfully Done.", "path", "DistributeRewards")
-	}
-
-}
-
-func (k Keeper) calculateTotalEarnings(ctx sdk.Context, currentEarnings string, earnedRewardsBigInt *big.Int, entityType string) (sdk.Coin, error) {
-	earnedAmount := sdk.NewIntFromBigInt(earnedRewardsBigInt)
-	earnedCoin := sdk.NewCoin(params.BondDenom, earnedAmount)
-
-	netEarnings, err := sdk.ParseCoinNormalized(currentEarnings)
-	if err != nil {
-		return sdk.Coin{}, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, errors.NetEarnings)
-	}
-
-	totalEarnings := netEarnings.Add(earnedCoin)
-
-	return totalEarnings, nil
+	// Now totalRewards contains the amount to be distributed to be used in the transaction flow
+	return totalRewards, nil
 }
