d2274da45215409e46085cf02d0c2c2b94b2e45a
golnar-m <golnar@soarrobotics.com>
2023-11-07 00:51:01 +0300

    feat(query): refactor to use GetChallengerDidUsingPubKey for effeciancy

--- x/did/keeper/grpc_query_get_challenger_did_by_pubkey.go
+++ x/did/keeper/grpc_query_get_challenger_did_by_pubkey.go
@@ -6,32 +6,21 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (k Keeper) GetChallengerDidByPubKey(goCtx context.Context, req *types.QueryGetChallengerDidByPubKeyRequest) (*types.QueryGetChallengerDidByPubKeyResponse, error) {
-	if req == nil {
-		return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "[GetChallengerByPubKey] failed. Invalid request: %T.", req)
+	if req == nil || req.Pubkey == "" {
+		return nil, status.Error(codes.InvalidArgument, "[GetChallengerDidByPubKey] failed. Invalid request.")
 	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	challengers := k.GetAllChallengerDid(ctx)
-	if len(challengers) == 0 || challengers == nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrNotFound, "[GetChallengerByPubKey][GetAllChallenger] failed. Couldn't find any challenger.")
+	challengers, found := k.GetChallengerDidUsingPubKey(ctx, req.Pubkey)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, "[GetChallengerDidByPubKey][GetRunnerDidUsingPubKey] failed. Couldn't find a valid challenger.")
 	}
 
-	targetChallenger := types.ChallengerDid{}
-
-	for _, challenger := range challengers {
-		if req.Pubkey == challenger.PubKey {
-			targetChallenger = challenger
-			break
-		}
-	}
-
-	if targetChallenger.PubKey == "" {
-		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidPubKey, "[GetChallengerByPubKey] failed. Couldn't find a valid public key for target challenger. got: [ %T ], Make sure public key is not empty OR invalid.", targetChallenger.PubKey)
-	}
-
-	return &types.QueryGetChallengerDidByPubKeyResponse{ChallengerDid: &targetChallenger}, nil
+	return &types.QueryGetChallengerDidByPubKeyResponse{ChallengerDid: &challengers}, nil
 }
