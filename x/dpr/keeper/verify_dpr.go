77cd3ad6303b131bfd8afb88d772b3ff6334f9f9
Alperen-Dere <alperen-dere@hotmail.com>
2023-11-08 09:41:16 +0300

    feat: new verification functions

--- x/dpr/keeper/verify_dpr.go
+++ x/dpr/keeper/verify_dpr.go
@@ -3,7 +3,7 @@ package keeper
 import (
 	"soarchain/app/params"
 	"soarchain/x/dpr/types"
-	utility "soarchain/x/dpr/utility"
+	"soarchain/x/dpr/utility"
 	"strings"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -11,7 +11,7 @@ import (
 
 func (k Keeper) VerifyDprInputs(msg *types.MsgGenDpr) bool {
 	return isValidCreator(msg) &&
-		isValidSupportedPIDs(msg) &&
+		isValidSupportedPIDs(*msg.GetSupportedPIDs()) &&
 		isValidDprBudget(msg) &&
 		isValidMaxClientCount(msg) &&
 		isValidName(msg)
@@ -22,9 +22,15 @@ func isValidCreator(msg *types.MsgGenDpr) bool {
 	return msg.Creator != ""
 }
 
-func isValidSupportedPIDs(msg *types.MsgGenDpr) bool {
-	// Check if hex is valid using regex
-	return utility.IsValidHex(msg.SupportedPIDs)
+func isValidSupportedPIDs(supportedPIDs types.SupportedPIDs) bool {
+	return utility.IsValidHex(supportedPIDs.Pid_1To_20) &&
+		utility.IsValidHex(supportedPIDs.Pid_21To_40) &&
+		utility.IsValidHex(supportedPIDs.Pid_41To_60) &&
+		utility.IsValidHex(supportedPIDs.Pid_61To_80) &&
+		utility.IsValidHex(supportedPIDs.Pid_81To_A0) &&
+		utility.IsValidHex(supportedPIDs.Pid_A1To_C0) &&
+		utility.IsValidHex(supportedPIDs.Pid_C1To_E0) &&
+		utility.IsValidHex(supportedPIDs.Pid_SVCTo_9)
 }
 
 func isValidDprBudget(msg *types.MsgGenDpr) bool {
@@ -52,10 +58,64 @@ func isValidName(msg *types.MsgGenDpr) bool {
 
 func (k Keeper) VerifyEnterDprInputs(msg *types.MsgEnterDpr) bool {
 
-	return isValidSupportedPID(msg)
+	return isValidSupportedPID(*msg.GetSupportedPIDs())
 }
 
-func isValidSupportedPID(msg *types.MsgEnterDpr) bool {
-	// Check if hex is valid using regex
-	return utility.IsValidHex(msg.SupportedPIDs)
+func isValidSupportedPID(supportedPIDs types.SupportedPIDs) bool {
+	return utility.IsValidHex(supportedPIDs.Pid_1To_20) &&
+		utility.IsValidHex(supportedPIDs.Pid_21To_40) &&
+		utility.IsValidHex(supportedPIDs.Pid_41To_60) &&
+		utility.IsValidHex(supportedPIDs.Pid_61To_80) &&
+		utility.IsValidHex(supportedPIDs.Pid_81To_A0) &&
+		utility.IsValidHex(supportedPIDs.Pid_A1To_C0) &&
+		utility.IsValidHex(supportedPIDs.Pid_C1To_E0) &&
+		utility.IsValidHex(supportedPIDs.Pid_SVCTo_9)
+}
+
+func IsCarSupportsDpr(carSupportedPIDs *types.SupportedPIDs, dprSupportedPIDs *types.SupportedPIDs) (bool, error) {
+	carSupportsPID1To20, err := utility.ArePIDsSupported(carSupportedPIDs.Pid_1To_20, dprSupportedPIDs.Pid_1To_20)
+	if err != nil {
+		return false, err
+	}
+
+	carSupportsPID21To40, err := utility.ArePIDsSupported(carSupportedPIDs.Pid_21To_40, dprSupportedPIDs.Pid_21To_40)
+	if err != nil {
+		return false, err
+	}
+
+	carSupportsPID41To60, err := utility.ArePIDsSupported(carSupportedPIDs.Pid_41To_60, dprSupportedPIDs.Pid_41To_60)
+	if err != nil {
+		return false, err
+	}
+
+	carSupportsPID61To80, err := utility.ArePIDsSupported(carSupportedPIDs.Pid_61To_80, dprSupportedPIDs.Pid_61To_80)
+	if err != nil {
+		return false, err
+	}
+
+	carSupportsPID81ToA0, err := utility.ArePIDsSupported(carSupportedPIDs.Pid_81To_A0, dprSupportedPIDs.Pid_81To_A0)
+	if err != nil {
+		return false, err
+	}
+
+	carSupportsPIDA1ToC0, err := utility.ArePIDsSupported(carSupportedPIDs.Pid_A1To_C0, dprSupportedPIDs.Pid_A1To_C0)
+	if err != nil {
+		return false, err
+	}
+
+	carSupportsPIDC1ToE0, err := utility.ArePIDsSupported(carSupportedPIDs.Pid_C1To_E0, dprSupportedPIDs.Pid_C1To_E0)
+	if err != nil {
+		return false, err
+	}
+
+	carSupportsPIDSvcTo9, err := utility.ArePIDsSupported(carSupportedPIDs.Pid_SVCTo_9, dprSupportedPIDs.Pid_SVCTo_9)
+	if err != nil {
+		return false, err
+	}
+
+	// The car supports the DPR if all required PIDs are supported.
+	isSupported := carSupportsPID1To20 && carSupportsPID21To40 && carSupportsPID41To60 && carSupportsPID61To80 &&
+		carSupportsPID81ToA0 && carSupportsPIDA1ToC0 && carSupportsPIDC1ToE0 && carSupportsPIDSvcTo9
+
+	return isSupported, nil
 }
