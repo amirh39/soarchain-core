e3b1ead4b8f29c279829df4758966bf2993c0363
Amir <a.khoshbakht79@gmail.com>
2023-11-10 20:51:30 +0300

    test(dpr): fix claim dpr rewards test suite

--- x/dpr/keeper/msg_server_claim_dpr_rewards_test.go
+++ x/dpr/keeper/msg_server_claim_dpr_rewards_test.go
@@ -1,10 +1,12 @@
 package keeper_test
 
 import (
-	"log"
+	"soarchain/app/params"
 	didtypes "soarchain/x/did/types"
 	"soarchain/x/dpr/keeper"
 	"soarchain/x/dpr/types"
+
+	poaTypes "soarchain/x/poa/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -12,39 +14,44 @@ import (
 func (helper *KeeperTestHelper) Test_Claim_DPR() {
 
 	helper.Run("TestClaimDpr", func() {
+		ctx := sdk.WrapSDKContext(helper.Ctx)
 		helper.Setup()
+		helper.MsgServer = keeper.NewMsgServerImpl(helper.App.DprKeeper)
 
+		bankKeeper := helper.App.BankKeeper
 		dprKeeper := helper.App.DprKeeper
 		didKeeper := helper.App.DidKeeper
-		helper.MsgServer = keeper.NewMsgServerImpl(helper.App.DprKeeper)
-		ctx := sdk.WrapSDKContext(helper.Ctx)
+		poaKeeper := helper.App.PoaKeeper
 
-		dpr := SetupSecondDpr(1)
-		dprKeeper.SetDpr(helper.Ctx, dpr[0])
-		helper.Require().NotEmpty(dpr)
+		targetDpr := SetupSecondDpr(1)
+		dprKeeper.SetDpr(helper.Ctx, targetDpr[0])
+		helper.Require().NotEmpty(targetDpr)
 
-		dprinfo := &didtypes.DprInfo{ // Note: create a pointer
-			Id:      DprId,
-			Claimed: "0",
+		mintAmount, _ := sdk.ParseCoinNormalized(targetDpr[0].DprBudget)
+		bankKeeper.MintCoins(helper.Ctx, types.ModuleName, sdk.NewCoins(sdk.NewCoin(params.BondDenom, sdk.Int(mintAmount.Amount))))
+
+		clientDid := didtypes.ClientDid{
+			Id:      Did,
+			PubKey:  PUBKEY,
+			Address: CREATOR,
+			DprInfos: []*didtypes.DprInfo{
+				{
+					Id:      targetDpr[0].Id,
+					Claimed: "0",
+				}},
 		}
-
-		newDid := didtypes.ClientDid{
-			Id:       Did,
-			PubKey:   PUBKEY,
-			Address:  ADDRESS,
-			DprInfos: []*didtypes.DprInfo{dprinfo},
+		didKeeper.SetClientDid(helper.Ctx, clientDid)
+		clientReputation := poaTypes.Reputation{
+			PubKey:      PUBKEY,
+			Address:     CREATOR,
+			DprEarnings: "0" + params.BondDenom,
 		}
-
-		didKeeper.SetClientDid(helper.Ctx, newDid)
-		client, _ := didKeeper.GetClientDid(helper.Ctx, ADDRESS)
-		log.Println(client)
+		poaKeeper.SetReputation(helper.Ctx, clientReputation)
 
 		res, err := helper.MsgServer.ClaimDprRewards(ctx, &types.MsgClaimDprRewards{
 			Sender: CREATOR,
 		})
 		helper.Require().Empty(res)
 		helper.Require().Nil(err)
-		dprC, _ := dprKeeper.GetDpr(helper.Ctx, DprID)
-		log.Println(dprC)
 	})
 }
